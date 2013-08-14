11471ca27a9ff4979a5a5365a2c77a87cd4d6d1c
P.C. Shyamshankar <sykora@lucentbeing.com>
2013-08-14 17:20:22 -0400

    Add default role injection to batch operation.

--- src/Language/K3/Driver/Batch.hs
+++ src/Language/K3/Driver/Batch.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE ViewPatterns #-}
 module Language.K3.Driver.Batch where
+
+import Control.Arrow
 
 import Data.Functor
 
 import System.IO
+
+import Language.K3.Core.Annotation
+import Language.K3.Core.Declaration
+
+import qualified Language.K3.Core.Constructor.Declaration as D
+import qualified Language.K3.Core.Constructor.Expression as E
+import qualified Language.K3.Core.Constructor.Type as T
 
 import Language.K3.Interpreter
 import Language.K3.Parser
@@ -16,6 +26,16 @@ openFileOrStdIn :: String -> IO Handle
 openFileOrStdIn "-" = return stdin
 openFileOrStdIn f = openFile f ReadMode
 
+setDefaultRole :: K3 Declaration -> String -> String -> K3 Declaration
+setDefaultRole (tag &&& children -> (DRole roleName, subDecls)) targetName newDefault
+    | roleName == targetName = D.role roleName (newDefaultDecl : filter matchRole subDecls)
+  where
+    newDefaultDecl = D.global "role" T.string (Just $ E.constant $ E.CString newDefault)
+    matchRole (tag -> DGlobal "role" _ _) = False
+    matchRole _ = True
+
+setDefaultRole d _ _ = d
+
 runBatch :: Options -> IO ()
 runBatch op = do
     h <- openFileOrStdIn $ input op
@@ -25,5 +45,6 @@ runBatch op = do
     case p of
         Left e -> putStrLn e
         Right q -> do
-            putStr $ pretty q
-            runProgram [defaultAddress] q
+            let q' = setDefaultRole q "__global" (defaultRole $ mode op)
+            putStr $ pretty q'
+            runProgram [defaultAddress] q'
