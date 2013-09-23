27f990606ccfc750e88ceea06ed41926e1bf12e1
P.C. Shyamshankar <shyam@cs.jhu.edu>
2013-09-23 14:47:53 -0400

    Move peer-specific option parsing from the driver.

---
+++ src/Language/K3/Runtime/Options.hs
@@ -0,0 +1,38 @@
+-- | Common option parsing for all K3 programs.
+module Language.K3.Runtime.Options where
+
+import Data.List.Split
+import Data.Maybe
+
+import Control.Applicative
+import Options.Applicative
+
+import qualified Language.K3.Core.Constructor.Expression as E
+
+import Language.K3.Parser
+
+import Language.K3.Runtime.Engine (Address(..), SystemEnvironment, PeerBootstrap)
+
+peerBReader :: String -> Either ParseError (Address, PeerBootstrap)
+peerBReader peerDesc = case splitOn ":" peerDesc of
+    (host:port:maps) -> Right (
+            Address (host, read port),
+            ("me", E.address (E.constant $ E.CString host) (E.constant $ E.CInt $ read port)) :
+            [(k, v) | mapping <- maps, let (k:s:_) = splitOn "=" mapping,
+                      let pv = parseExpression s,
+                      isJust pv,
+                      let v = fromJust pv
+            ]
+        )
+    _ -> Left $ ErrorMsg "Invalid Peer Parse"
+
+peerBOptions :: Parser (Address, PeerBootstrap)
+peerBOptions = nullOption (
+        short 'p'
+     <> long "peer"
+     <> reader peerBReader
+     <> help "Peer configuration in the format role:host:port."
+    )
+
+sysEnvOptions :: Parser SystemEnvironment
+sysEnvOptions = some peerBOptions
