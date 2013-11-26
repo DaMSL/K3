6f6824a374b75c1d8e1445973b0af498fada75d6
P.C. Shyamshankar <shyam@cs.jhu.edu>
2013-11-26 14:06:22 -0500

    Hoist signature function from L.K3.CG.Haskell.

--- src/Language/K3/Codegen/Common.hs
+++ src/Language/K3/Codegen/Common.hs
@@ -7,15 +7,18 @@ module Language.K3.Codegen.Common (
     annotationComboIdE,
     annotationComboIdL,
 
-    recordName
+    recordName,
+    signature
 ) where
+
+import Control.Arrow ((&&&))
 
 import Language.K3.Core.Common
 
-import Language.K3.Core.Annotation (K3, Annotation)
+import Language.K3.Core.Annotation (K3, Annotation, tag, children, annotations)
 import Language.K3.Core.Expression (Expression, namedEAnnotations)
 import Language.K3.Core.Literal (Literal, namedLAnnotations)
-import Language.K3.Core.Type (Type, namedTAnnotations)
+import Language.K3.Core.Type
 
 import Data.List
 
@@ -39,4 +42,33 @@ annotationComboIdL (namedLAnnotations -> ids) = Just $ annotationComboId ids
 -- Record Identifiers
 
 recordName :: [(Identifier, K3 Type)] -> Identifier
-recordName = undefined
+recordName its = concat $ fst $ unzip its
+
+signature :: Monad m => K3 Type -> m Identifier
+signature (tag -> TBool)        = return "P"
+signature (tag -> TByte)        = return "B"
+signature (tag -> TInt)         = return "N"
+signature (tag -> TReal)        = return "D"
+signature (tag -> TString)      = return "S"
+signature (tag -> TAddress)     = return "A"
+
+signature (tag &&& children -> (TOption, [x])) = signature x >>= return . ("O" ++)
+
+signature (tag &&& children -> (TTuple, ch)) =
+  mapM signature ch >>= return . (("T" ++ (show $ length ch)) ++) . concat
+
+signature (tag &&& children -> (TRecord ids, ch)) =
+  mapM signature ch
+  >>= return . intercalate "_" . map (\(x,y) -> (sanitize x) ++ "_" ++ y) . zip ids
+  >>= return . (("R" ++ (show $ length ch)) ++)
+
+signature (tag &&& children -> (TIndirection, [x])) = signature x >>= return . ("I" ++)
+signature (tag &&& children -> (TCollection, [x]))  = signature x >>= return . ("C" ++)
+
+signature (tag &&& children -> (TFunction, [a,r]))  = signature a >>= \s -> signature r >>= return . (("F" ++ s) ++)
+signature (tag &&& children -> (TSink, [x]))        = signature x >>= return . ("K" ++)
+signature (tag &&& children -> (TSource, [x]))      = signature x >>= return . ("U" ++)
+
+signature (tag &&& children -> (TTrigger, [x])) = signature x >>= return . ("G" ++)
+
+sanitize = id
