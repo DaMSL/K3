18de0a51dff460e8d60134072535291bb10af5cd
P.C. Shyamshankar <shyam@cs.jhu.edu>
2014-03-31 10:36:02 -0400

    Reintroduce the imperative phase in the CPP pipeline.

--- src/Language/K3/Compiler/CPP.hs
+++ src/Language/K3/Compiler/CPP.hs
@@ -9,7 +9,7 @@ import Text.PrettyPrint.ANSI.Leijen
 
 import Language.K3.TypeSystem (typecheckProgram)
 
-import qualified Language.K3.Codegen.Imperative as IMP
+import qualified Language.K3.Codegen.Imperative as I
 import qualified Language.K3.Codegen.CPP as CPP
 
 import Language.K3.Driver.Common (parseK3Input)
@@ -26,17 +26,15 @@ compile opts copts = do
             let (typeErrors, _, typedProgram) = typecheckProgram d
             if not (S.null typeErrors)
                 then putStrLn $ prettyTCErrors typedProgram typeErrors
-                else do
-                    let (i, _) = IMP.runImperativeM (IMP.declaration typedProgram) ()
-                    case i of
-                        Left () -> print "Imperative Transformation Error"
-                        Right i' -> do
-                            let (r, _) = CPP.runCPPGenM CPP.defaultCPPGenS (CPP.program i')
-                            case r of
-                                Left e -> print e
-                                Right s -> case buildDir copts of
-                                    Nothing -> print "Error: No build directory specified."
-                                    Just b -> do
-                                        createDirectoryIfMissing True b
-                                        let oFile = joinPath [b, replaceExtension (takeBaseName $ input opts) "cpp"]
-                                        writeFile oFile (displayS (renderPretty 1.0 100 s) "")
+                else let (ir, _) = I.runImperativeM (I.declaration typedProgram) I.defaultImperativeS in
+                    case ir of
+                      Left () -> print "Error in Imperative Transformation."
+                      Right ir' -> let (r, _) = CPP.runCPPGenM CPP.defaultCPPGenS (CPP.program ir') in
+                          case r of
+                              Left e -> print e
+                              Right s -> case buildDir copts of
+                                  Nothing -> print "Error: No build directory specified."
+                                  Just b -> do
+                                      createDirectoryIfMissing True b
+                                      let oFile = joinPath [b, replaceExtension (takeBaseName $ input opts) "cpp"]
+                                      writeFile oFile (displayS (renderPretty 1.0 100 s) "")
