ec2903600de2f58c4486fd372ab7842749805123
Yotam Barnoy <yotambarnoy@gmail.com>
2014-04-17 20:35:12 -0400

    Updated sanitize_log

--- scripts/cumulus/sanitize_log.py
+++ scripts/cumulus/sanitize_log.py
@@ -7,11 +7,11 @@ def reduce_region(lines):
   take = False
   for line in lines:
     if not take:
-      if re.match("^TRIGGER .*$", line):
+      if re.match("^ *TRIGGER .*$", line):
         take = True
         out += [line]
     elif take:
-      if re.match("^Mode:.*$", line) or re.match("^EVENT LOOP.*$", line):
+      if re.match("^Mode:.*$", line) or re.match("^ *EVENT LOOP.*$", line):
         take = False
       else:
         out += [line]
@@ -21,7 +21,7 @@ def group_triggers(lines):
   out = []
   group = []
   for line in lines:
-    if re.match("^TRIGGER .*$", line):
+    if re.match("^ *TRIGGER .*$", line):
       if group != []:
         out += [group]
       group = [line]
@@ -31,18 +31,44 @@ def group_triggers(lines):
     out += [group]
   return out
 
+# Take a group of lines representing a trigger and convert them to proper output
+def lines_of_trig(trig):
+  mobj =  re.match(r'^TRIGGER (.+) (.+) {', trig[0])
+  if mobj:
+    name, addr = mobj.group(1), mobj.group(2)
+    if name and addr:
+      mobj = re.match(r'^Args: (\(.*\))$', trig[1])
+      args = mobj.group(1)
+      if args:
+        pairs = [("args", args)]
+        for l in trig[2:]:
+          mobj = re.match(r'^(.*) => (.*)$', l)
+          if mobj:
+            pairs.append((mobj.group(1), mobj.group(2)))
+        # convert pairs to lines
+        out = ['/'.join([addr, name, pair[0], pair[1]]) for pair in pairs]
+        return out
+
+# clean a line by removing initial whitespace, \n, long whitespace
+def clean(line):
+  line = line.replace('\n','')
+  line = re.sub(r'\s+', ' ', line)
+  line = re.sub(r'^ ', '', line)
+  return line
+
 def main():
   with open("results.txt", "r") as f:
     if f:
       lines = f.readlines()
-      lines = list(map(lambda l: l.replace('\n',''),lines))
+      lines = [clean(l) for l in lines]
       lines = reduce_region(lines)
       trigs = group_triggers(lines)
-      print(trigs)
-      #s = ""
-      #for l in lines:
-        #s += l
-    #print(s)
+      trigs = [lines_of_trig(t) for t in trigs]
+      out = []
+      for t in trigs:
+        out += t
+      out = '\n'.join(out)
+      print(out)
 
 if __name__ == '__main__':
   main()
