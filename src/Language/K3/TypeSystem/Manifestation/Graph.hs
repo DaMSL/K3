ea3b7e11e399017fc5476d81beffee6fa59c9fbe
P.C. Shyamshankar <shyam@cs.jhu.edu>
2014-05-05 14:10:03 -0400

    Split narrowAndReduce to separate function.

--- src/Language/K3/TypeSystem/Manifestation/Graph.hs
+++ src/Language/K3/TypeSystem/Manifestation/Graph.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE PatternGuards #-}
+
 -- | Dependent type-manifestation using graph traversal.
 --
 -- This module defines an algorithm and supporting machinery to perform dependent type manifestation
@@ -8,7 +10,8 @@
 -- such that the manifest types still satisfy their constraints.
 module Language.K3.TypeSystem.Manifestation.Graph where
 
-import Data.Maybe (fromJust)
+import Data.Maybe
+import Data.Tuple (swap)
 
 import qualified Data.Graph.Wrapper as G
 import qualified Data.Map as M
@@ -31,31 +34,37 @@ type ManifestGraph = G.Graph (S.Set UID) (BoundType, K3 Type, K3 Type)
 --
 -- This involves creating a sanitized version of the constraint set (containing only source
 -- variables) and turning constraints into edges.
-fromTypecheckResult :: TypecheckResult -> Maybe (M.Map Int (S.Set (Int, Int)))
+fromTypecheckResult :: TypecheckResult -> Maybe (M.Map UID (S.Set (UID, UID)))
 fromTypecheckResult result = case tcExprTypes result of
     Nothing -> Nothing
     Just (tVarMap, constraintSet) -> Just $ narrowAndReduce tVarMap (indexBoundingConstraintsByUVar constraintSet)
+
+narrowAndReduce :: M.Map UID AnyTVar -> M.Map UVar (S.Set Constraint) -> M.Map UID (S.Set (UID, UID))
+narrowAndReduce tVarMap cm = M.fromList
+            [ (t, ncs)
+            | (t, SomeUVar tv) <- M.toList tVarMap
+            , let (Just wcs) = M.lookup tv cm
+            , let ncs = S.fromList . catMaybes $ map sanitizeIntermediateConstraint (S.toList wcs)
+            -- , let mcs = S.map (translatePair (reverseMap tVarMap) . intermediateConstraintToPair) ncs
+            ]
   where
-    narrowAndReduce :: M.Map UID AnyTVar -> M.Map UVar (S.Set Constraint) -> M.Map Int (S.Set (Int, Int))
-    narrowAndReduce tvm cm = M.fromList
-                [ (t, mcs)
-                | (UID t, SomeUVar tv) <- M.toList tvm
-                , let (Just wcs) = M.lookup tv cm
-                , let ncs = S.filter (isValidIntermediateConstraint $ reverseMap tvm) wcs
-                , let mcs = S.map (translatePair (reverseMap tvm) . intermediateConstraintToPair) ncs
-                ]
+    -- | A reverse mapping, from basic IDs to UIDs, for all the UIDs we care about.
+    basicIDMap :: M.Map Int UID
+    basicIDMap = M.fromList [(tvarIdNum (anyTVarId t), uid) | (uid, t) <- M.toList tVarMap]
+
     translatePair :: M.Map Int Int -> (Int, Int) -> (Int, Int)
     translatePair m (i, j) = (fromJust $ M.lookup i m, fromJust $ M.lookup j m)
 
-    reverseMap :: M.Map UID AnyTVar -> M.Map Int Int
-    reverseMap = M.fromList . map (\(UID i, SomeUVar u) -> (tvarIdNum $ tvarId u, i)) . M.toList
+    sanitizeIntermediateConstraint :: Constraint -> Maybe (UID, UID)
+    sanitizeIntermediateConstraint (IntermediateConstraint (CRight u) (CRight v))
+        | Just uUID <- M.lookup ubid basicIDMap, Just vUID <- M.lookup vbid basicIDMap = Just (uUID, vUID)
+        | otherwise = Nothing
+      where
+        ubid = tvarIdNum $ tvarId u
+        vbid = tvarIdNum $ tvarId v
+    sanitizeIntermediateConstraint _ = Nothing
 
     intermediateConstraintToPair :: Constraint -> (Int, Int)
     intermediateConstraintToPair (IntermediateConstraint (CRight u) (CRight v)) =
         (tvarIdNum $ tvarId u, tvarIdNum $ tvarId v)
     intermediateConstraintToPair _ = undefined
-
-    isValidIntermediateConstraint :: M.Map Int Int -> Constraint -> Bool
-    isValidIntermediateConstraint m (IntermediateConstraint (CRight u) (CRight v)) =
-        M.member (tvarIdNum (tvarId u)) m && M.member (tvarIdNum $ tvarId v) m
-    isValidIntermediateConstraint _ _ = False
