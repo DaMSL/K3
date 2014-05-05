2e6654125c340e69062a6b19137f88f2c4fdf971
P.C. Shyamshankar <shyam@cs.jhu.edu>
2014-05-05 15:18:43 -0400

    Construct consolidated ManifestGraph.
    This version produces an actual payload for each vertex, but with
    LowerBounds everywhere. Picking the actual bound needs to come from
    inspecting the arrows themselves.

--- src/Language/K3/TypeSystem/Manifestation/Graph.hs
+++ src/Language/K3/TypeSystem/Manifestation/Graph.hs
@@ -10,7 +10,6 @@
 -- such that the manifest types still satisfy their constraints.
 module Language.K3.TypeSystem.Manifestation.Graph where
 
-import Data.Functor
 import Data.Maybe
 import Data.Tuple (swap)
 
@@ -35,20 +34,31 @@ type ManifestGraph = G.Graph (S.Set UID) (BoundType, K3 Type, K3 Type)
 --
 -- This involves creating a sanitized version of the constraint set (containing only source
 -- variables) and turning constraints into edges.
-fromTypecheckResult :: TypecheckResult -> Maybe (G.Graph (S.Set UID) (S.Set UID))
-fromTypecheckResult result = consolidateGraph <$> tcExprTypes result
+fromTypecheckResult :: TypecheckResult -> Maybe ManifestGraph
+fromTypecheckResult result = do
+    (tVarMap, constraintSet) <- tcExprTypes result
+    boundsMap <- tcExprBounds result
+
+    let narrowedConstraintMap = narrowAndReduce tVarMap (indexBoundingConstraintsByUVar constraintSet)
+    let consolidatedVertices = attachPayload boundsMap narrowedConstraintMap
+    let consolidatedEdges = S.toList . S.unions $ M.elems narrowedConstraintMap
+
+    return $ G.fromVerticesEdges consolidatedVertices consolidatedEdges
   where
-    consolidateGraph (tvm, cs) = constructGraph $ narrowAndReduce tvm (indexBoundingConstraintsByUVar cs)
-    constructGraph m = G.fromVerticesEdges (zip (M.keys m) (M.keys m)) (S.toList $ S.unions $ M.elems m)
+    attachPayload bm ncm =
+        [ (u, (LowerBound, lb, ub))
+        | u <- M.keys ncm
+        , let Just (lb, ub) = M.lookup (S.findMin u) bm
+        ]
 
 narrowAndReduce :: M.Map UID AnyTVar -> M.Map UVar (S.Set Constraint)
                 -> M.Map (S.Set UID) (S.Set (S.Set UID, S.Set UID))
 narrowAndReduce tVarMap cm = M.fromList
-            [ (uset, ncs)
-            | (atvar, uset) <- M.toList reverseMap
-            , let (Just wcs) = M.lookup (fromJust $ onlyUVar atvar) cm
-            , let ncs = S.fromList . catMaybes $ map sanitizeIntermediateConstraint (S.toList wcs)
-            ]
+        [ (uset, ncs)
+        | (atvar, uset) <- M.toList reverseMap
+        , let (Just wcs) = M.lookup (fromJust $ onlyUVar atvar) cm
+        , let ncs = S.fromList . catMaybes $ map sanitizeIntermediateConstraint (S.toList wcs)
+        ]
   where
     -- | A reverse mapping, from basic IDs to UIDs, for all the UIDs we care about.
     reverseMap :: M.Map AnyTVar (S.Set UID)
@@ -57,17 +67,9 @@ narrowAndReduce tVarMap cm = M.fromList
     collapseReverseMap :: (Ord k, Ord v) => M.Map k v -> M.Map v (S.Set k)
     collapseReverseMap = M.fromListWith S.union . map (fmap S.singleton . swap) . M.toList
 
-    translatePair :: M.Map Int Int -> (Int, Int) -> (Int, Int)
-    translatePair m (i, j) = (fromJust $ M.lookup i m, fromJust $ M.lookup j m)
-
     sanitizeIntermediateConstraint :: Constraint -> Maybe (S.Set UID, S.Set UID)
     sanitizeIntermediateConstraint (IntermediateConstraint (CRight u) (CRight v))
         | Just uUID <- M.lookup (someVar u) reverseMap
         , Just vUID <- M.lookup (someVar v) reverseMap = Just (uUID, vUID)
         | otherwise = Nothing
     sanitizeIntermediateConstraint _ = Nothing
-
-    intermediateConstraintToPair :: Constraint -> (Int, Int)
-    intermediateConstraintToPair (IntermediateConstraint (CRight u) (CRight v)) =
-        (tvarIdNum $ tvarId u, tvarIdNum $ tvarId v)
-    intermediateConstraintToPair _ = undefined
