af6d6837257e6ef955662bf175c0c52bfdcb4173
Yotam Barnoy <yotambarnoy@gmail.com>
2014-05-05 14:53:25 -0400

    Updates and fixes to sanitize_log

--- scripts/cumulus/sanitize_log.py
+++ scripts/cumulus/sanitize_log.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+from optparse import OptionParser
 import re
+import sys
+from os.path import basename
 
+# focus on the region we care about in the log file
 def reduce_region(lines):
   out = []
   take = False
@@ -32,7 +36,7 @@ def group_triggers(lines):
   return out
 
 # Take a group of lines representing a trigger and convert them to proper output
-def lines_of_trig(trig):
+def lines_of_trig(log_name, trig_num, trig):
   mobj =  re.match(r'^TRIGGER (.+) (.+) {', trig[0])
   if mobj:
     name, addr = mobj.group(1), mobj.group(2)
@@ -50,6 +54,9 @@ def lines_of_trig(trig):
             pairs.append((lastkey, lastval))
           lastkey = mobj.group(1)
           lastval = mobj.group(2)
+          # check for weird args var, and translate Args to args
+          if i == 1 and lastkey == 'Args': lastkey = 'args'
+          elif lastkey == 'args': lastkey = None
         else: # maybe a continued line?
           mobj = re.match(r'^.. (.*)$', line)
           if mobj:
@@ -64,7 +71,7 @@ def lines_of_trig(trig):
         pairs.append((lastkey, lastval))
 
       # convert pairs to lines
-      out = ['/'.join([addr, name, pair[0], pair[1]]) for pair in pairs]
+      out = ['/'.join([log_name, str(trig_num), addr, name, pair[0], pair[1]]) for pair in pairs]
       return out
 
 # clean a line by removing initial whitespace, \n, long whitespace
@@ -74,20 +81,30 @@ def clean(line):
   line = re.sub(r'^ ', '', line)
   return line
 
-def main():
-  with open("results.txt", "r") as f:
+def main(log_name, filename):
+  with open(filename, "r") as f:
     if f:
       lines = f.readlines()
       lines = [clean(l) for l in lines]
       lines = reduce_region(lines)
       trigs = group_triggers(lines)
-      trigs = [lines_of_trig(t) for t in trigs]
+      trigs = [lines_of_trig(log_name, i, t) for i, t in enumerate(trigs, start=1)]
       out = []
       for t in trigs:
         out += t
       out = '\n'.join(out)
       print(out)
+    else:
+      print("File {filename} not found".format(**locals()))
 
 if __name__ == '__main__':
-  main()
+  usage = "Usage: {0} [options] <log file>".format(basename(sys.argv[0]))
+  parser = OptionParser(usage=usage)
+  parser.add_option("-n", "--name", type="string", dest="log_name",
+                    default="log", help="Specify desired name of log (for comparing different logs)")
+  (options, args) = parser.parse_args()
+  if args:
+    main(options.log_name, args[0])
+  else:
+    print usage
 
