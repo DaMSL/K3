3bf1647edcafb184a4c654d5ddf42cf57f2a2723
Yanif Ahmad <yanif@jhu.edu>
2014-05-13 12:13:35 -0400

    Added program effect normalization

---
+++ src/Language/K3/Transform/Normalization.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE ViewPatterns #-}
+
+-- Program normalization for effectful control expressions.
+module Language.K3.Transform.Normalization where
+
+import Control.Arrow
+import Control.Monad.Identity
+
+import Data.Tree
+
+import Language.K3.Core.Annotation
+import Language.K3.Core.Declaration
+import Language.K3.Core.Expression
+
+normalizeProgram :: K3 Declaration -> K3 Declaration
+normalizeProgram p = runIdentity $ mapTree normalizeDecl p
+  where normalizeDecl ch (tag &&& annotations -> (DGlobal i t eOpt, anns)) = do
+          let neOpt = maybe Nothing (Just . normalizeExpr) eOpt
+          return $ Node (DGlobal i t neOpt :@: anns) ch
+
+        normalizeDecl ch (tag &&& annotations -> (DTrigger i t e, anns)) = do
+          let ne = normalizeExpr e
+          return $ Node (DTrigger i t ne :@: anns) ch
+
+        normalizeDecl ch (tag &&& annotations -> (DAnnotation i tVars mems, anns)) = do
+          let nMems = normalizeAnnotationMembers mems
+          return $ Node (DAnnotation i tVars nMems :@: anns) ch
+
+        normalizeDecl ch (Node t _) = return $ Node t ch
+
+normalizeExpr :: K3 Expression -> K3 Expression
+normalizeExpr expr = uncurry rebuildBlock $ runIdentity $ foldMapRebuildTree normalize [] expr
+  where
+    normalize _ ch n@(tag -> EConstant c) = return $ ([], Node (EConstant c :@: annotations n) ch)
+    normalize _ ch n@(tag -> EVariable i) = return $ ([], Node (EVariable i :@: annotations n) ch)
+
+    normalize acc ch n@(tag -> EOperate OSeq) = return $ (nEffects, last ch)
+      where nEffects = head acc ++ [(head ch, annotations n)] ++ last acc
+
+    normalize acc ch n@(liftAll   -> True) = liftAllEffects  acc ch n
+    normalize acc ch n@(liftFirst -> True) = liftFirstEffect acc ch n
+    normalize acc ch n@(liftNone  -> True) = liftNoEffects   acc ch n
+    normalize _ _ n = error $ "Invalid pattern match in effect normalization: " ++ show n
+
+    -- | Fully chained effect normalization, where all children lift and preserve
+    --   the order of their effects.
+    liftAll (tag -> ESome)         = True
+    liftAll (tag -> EIndirect)     = True
+    liftAll (tag -> ETuple)        = True
+    liftAll (tag -> ERecord _)     = True
+    liftAll (tag -> EAssign _)     = True
+    liftAll (tag -> EProject _)    = True
+    liftAll (tag -> EAddress)      = True
+    liftAll (tag -> ESelf)         = True
+    liftAll (tag -> EOperate OSeq) = False
+    liftAll (tag -> EOperate _)    = True
+    liftAll _ = False
+
+    liftFirst (tag -> ELetIn _)  = True
+    liftFirst (tag -> EBindAs _) = True
+    liftFirst (tag -> ECaseOf _) = True
+    liftFirst _ = False
+
+    liftNone (tag -> ELambda _)   = True
+    liftNone (tag -> EIfThenElse) = True
+    liftNone _ = False
+
+    liftAllEffects  acc ch n = return $ (concat acc, Node (tag n :@: annotations n) ch)
+    liftFirstEffect acc ch n = return $ (head acc, Node (tag n :@: annotations n) $ rebuildTailChildren acc ch)
+    liftNoEffects   acc ch n = return $ ([], rebuildBlock (concat acc) $ Node (tag n :@: annotations n) ch)
+
+    rebuildTailChildren acc ch = (head ch):(map (uncurry rebuildBlock) $ zip (tail acc) $ tail ch)
+    rebuildBlock effects e = foldr (\(lE, anns) rE -> Node (EOperate OSeq :@: anns) [lE, rE]) e effects
+
+normalizeAnnotationMembers :: [AnnMemDecl] -> [AnnMemDecl]
+normalizeAnnotationMembers mems = map normalizeMember mems
+  where normalizeMember (Lifted    p n t eOpt anns) = Lifted      p n t (normalizeInit eOpt) anns
+        normalizeMember (Attribute p n t eOpt anns) = Attribute   p n t (normalizeInit eOpt) anns
+        normalizeMember (MAnnotation p n anns)      = MAnnotation p n anns
+        normalizeInit eOpt = maybe Nothing (Just . normalizeExpr) eOpt
