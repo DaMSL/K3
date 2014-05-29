465f9799519b14dd47933d86d9d3d5a1115b7a2a
Yotam Barnoy <yotambarnoy@gmail.com>
2014-05-29 14:17:34 -0400

    Fix compilation

--- src/Language/K3/Transform/Common.hs
+++ src/Language/K3/Transform/Common.hs
@@ -14,6 +14,7 @@ import Language.K3.Core.Annotation
 import Language.K3.Core.Common
 import Language.K3.Core.Declaration
 import Language.K3.Core.Expression
+import Language.K3.Analysis.Common
 
 import qualified Language.K3.Core.Constructor.Expression as EC
 
