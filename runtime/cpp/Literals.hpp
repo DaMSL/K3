7760ffc74d6aa6ffa70beda09af26b298a667d30
P.C. Shyamshankar <shyam@cs.jhu.edu>
2014-06-02 17:54:57 -0400

    Add tuple, record and option parsing rules.

--- runtime/cpp/Literals.hpp
+++ runtime/cpp/Literals.hpp
@@ -18,23 +18,38 @@ namespace K3 {
   class literal: public qi::grammar<iterator, map<string, string>(), qi::space_type> {
    public:
     literal(): literal::base_type(start) {
+      start = binding % ';';
+      binding = key >> '=' >> value;
+
       key = qi::char_("a-zA-Z_") >> *qi::char_("a-zA-Z0-9_");
-      value = quoted_string | +qi::char_("a-zA-Z0-9_");
-      assignment = key >> '=' >> value;
-      start = assignment % ';';
+      value
+        = qi::raw[qi::double_]
+        | qi::raw[parse_string]
+        | qi::raw[parse_option]
+        | qi::raw[parse_tuple]
+        | qi::raw[parse_record]
+        | key;
 
       escape = (qi::lit('\\') >> qi::char_) | qi::char_;
-      quoted_string = qi::lit('"') >> *(escape - '"') >> qi::lit('"');
+
+      parse_string = qi::lit('"') >> *(escape - '"') >> qi::lit('"');
+      parse_option = qi::string("none") | (qi::string("some") >> value);
+      parse_tuple = qi::lit('(') >> qi::raw[value % ','] >> qi::lit(')');
+      parse_record = qi::lit('{') >> qi::raw[(key >> ':' >> value) % ','] >> qi::lit('}');
     }
 
    private:
+    qi::rule<iterator, map<string, string>(), qi::space_type> start;
+    qi::rule<iterator, pair<string, string>(), qi::space_type> binding;
     qi::rule<iterator, string(), qi::space_type> key;
     qi::rule<iterator, string(), qi::space_type> value;
-    qi::rule<iterator, pair<string, string>(), qi::space_type> assignment;
-    qi::rule<iterator, map<string, string>(), qi::space_type> start;
 
-    qi::rule<iterator, string()> quoted_string;
     qi::rule<iterator, char()> escape;
+
+    qi::rule<iterator, string()> parse_string;
+    qi::rule<iterator, string(), qi::space_type> parse_tuple;
+    qi::rule<iterator, string(), qi::space_type> parse_record;
+    qi::rule<iterator, string(), qi::space_type> parse_option;
   };
 }
 
