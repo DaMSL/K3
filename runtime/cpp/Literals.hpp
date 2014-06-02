3f956e9ac426dbae98e116af4f1b72604845e04f
P.C. Shyamshankar <shyam@cs.jhu.edu>
2014-06-02 16:46:17 -0400

    Add proper support for identifiers and quoted strings.

--- runtime/cpp/Literals.hpp
+++ runtime/cpp/Literals.hpp
@@ -18,17 +18,23 @@ namespace K3 {
   class literal: public qi::grammar<iterator, map<string, string>(), qi::space_type> {
    public:
     literal(): literal::base_type(start) {
-      key = +qi::char_("a-zA-Z0-9_");
-      value = +qi::char_("a-zA-Z0-9_");
+      key = qi::char_("a-zA-Z_") >> *qi::char_("a-zA-Z0-9_");
+      value = quoted_string | +qi::char_("a-zA-Z0-9_");
       assignment = key >> '=' >> value;
       start = assignment % ';';
+
+      escape = (qi::lit('\\') >> qi::char_) | qi::char_;
+      quoted_string = qi::lit('"') >> *(escape - '"') >> qi::lit('"');
     }
 
    private:
-    qi::rule<iterator, string()> key;
-    qi::rule<iterator, string()> value;
+    qi::rule<iterator, string(), qi::space_type> key;
+    qi::rule<iterator, string(), qi::space_type> value;
     qi::rule<iterator, pair<string, string>(), qi::space_type> assignment;
     qi::rule<iterator, map<string, string>(), qi::space_type> start;
+
+    qi::rule<iterator, string()> quoted_string;
+    qi::rule<iterator, char()> escape;
   };
 }
 
