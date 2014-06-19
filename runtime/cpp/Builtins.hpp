dce6ef4016884e563df8df7a7614cc0a09e0ce54
jwheels <joshdub223@gmail.com>
2014-06-19 18:33:55 -0400

    Implement builtins for file sources

--- runtime/cpp/Builtins.hpp
+++ runtime/cpp/Builtins.hpp
@@ -6,27 +6,57 @@
 #include <functional>
 
 namespace K3 {
-  class Builtins: public __k3_context {
-    public:
+  //class Builtins: public __k3_context {
+  //  public:
 
-      Builtins() { 
-        // seed the random number generator
-        std::srand(std::time(0)); 
-      }
+  //    Builtins() {
+  //      // seed the random number generator
+  //      std::srand(std::time(0));
+  //    }
 
-      int random(int x) { return std::rand(x); }
+  //    int random(int x) { return std::rand(x); }
 
-      double randomFraction(unit_t) { return (double)rand() / RAND_MAX; }
+  //    double randomFraction(unit_t) { return (double)rand() / RAND_MAX; }
 
-      <template t>
-        int hash(t x) { return static_cast<int>std::hash<t>(x); }
+  //    <template t>
+  //      int hash(t x) { return static_cast<int>std::hash<t>(x); }
 
-      int truncate(double x) { return (int)x; }
+  //    int truncate(double x) { return (int)x; }
 
-      double real_of_int(int x) { return (double)x; }
+  //    double real_of_int(int x) { return (double)x; }
 
-      int get_max_int(unit_t x) { return INT_MAX; }
+  //    int get_max_int(unit_t x) { return INT_MAX; }
+  //};
+
+  template <class r> using F = std::function<r>;
+
+  F<F<F<unit_t(string)>(string)>(string)> openBuiltin =
+    [] (string chan_id) {
+      return [=] (string builtin_chan_id) {
+        return [=] (string format) {
+          engine.openBuiltin(chan_id, builtin_chan_id);
+          return unit_t();
+        };
+      };
+    };
+
+  F<F<F<F<unit_t(string)>(string)>(string)>(string)> openFile =
+    [] (string chan_id) {
+      return [=] (string path) {
+          return [=] (string fmt) {
+              return [=] (string mode) {
+                  IOMode iomode = engine.ioMode(mode);
+                  engine.openFile(chan_id, path, iomode);
+                  std::cout << "opened file!" << std::endl;
+                  return unit_t();
+              };
+          };
+      };
   };
 
+  unit_t close(string chan_id) {
+      engine.close(chan_id);
+      return unit_t();
+  }
 }
 #endif /* K3_RUNTIME_BUILTINS_H */
