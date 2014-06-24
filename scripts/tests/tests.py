b05089ec9046b1ceec9c3fb4771968064d711487
Yotam Barnoy <yotambarnoy@gmail.com>
2014-06-24 16:28:09 -0400

    tests: Added ability to read a list of tests, improved fail detection

--- scripts/tests/tests.py
+++ scripts/tests/tests.py
@@ -19,21 +19,25 @@ root_path = os.path.join(script_path, '../../../')
 def find_error(file):
     with open(file, 'r') as f:
         s = f.read()
-        if re.search(r'(Error|unexpected|No such file)', s):
+        if re.search(r'(Error|unexpected|No such file|Invalid)', s):
             return True
         else:
             return False
 
-def test_file(count, file):
+def test_file(count, file, verbose=False):
     temp = temp_file
     k3_path = os.path.join(root_path, './K3-Driver/dist/build/k3/k3')
     lib_path = os.path.join(root_path, './K3-Core/lib/k3')
     print('[{count}] Testing {file}...'.format(**locals()), end="")
     cmd = r'{k3_path} -I {lib_path} interpret -b -p 127.0.0.1:40000:role=\"s1\" {file} > {temp} 2>&1'.format(**locals())
-    # print(cmd)
+    if verbose:
+        print(cmd)
     os.system(cmd)
     if find_error(temp_file):
-        print("[ERROR]")
+        print("[ERROR]") 
+        if verbose:
+            with open(temp_file, 'r') as f:
+                print(f.read())
         return False
     else:
         print("[OK]")
@@ -41,29 +45,43 @@ def test_file(count, file):
 
 def find_test_files():
     file_list = []
-    counter = 1
-    failed = 0
     for dir in test_paths:
         d = os.path.join(root_path, dir)
         for file in os.listdir(d):
             if file.endswith(".k3"):
-                error = not test_file(counter, os.path.join(d, file))
-                counter += 1
-                failed += 1 if error else 0
-    count = counter - 1
+                file_list += [os.path.join(root_path, dir, file)]
+    return file_list
+
+def run_tests(file_list, verbose=False):
+    failed = 0
+    for i, f in enumerate(file_list):
+        if not test_file(i + 1, f, verbose):
+            failed += 1
+
     if failed > 0:
-        print('[{failed}/{count}] tests failed.'.format(**locals()))
+        print('[{0}/{1}] tests failed.'.format(failed, len(file_list)))
         return False
     else:
         print('All tests passed')
         return True
 
 def main(options):
+    file_list = []
+    # handle single files
     if options.test_file:
-        ret = test_file(1, options.test_file)
-    else:
-        ret = find_test_files()
-    if not ret:
+        file_list += [options.test_file]
+    # Handle a list in a file
+    if options.list_file:
+        files = []
+        with open(options.list_file, 'r') as f:
+            files = f.readlines()
+        for x in files:
+            file_list += [os.path.join(root_path, x)]
+
+    if len(file_list) == 0:
+        file_list = find_test_files()
+
+    if not run_tests(file_list, options.verbose):
         sys.exit(1)
 
 if __name__ == '__main__':
@@ -71,6 +89,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(usage=usage)
     parser.add_argument("-f", "--file", type=str, dest="test_file",
                         default=None, help="Specify path to a specific k3 test file")
+    parser.add_argument("-l", "--list", type=str, dest="list_file",
+                        default=None, help="Specify path to a file containing a list of test files")
+    parser.add_argument("-v", "--verbose", action='store_true', dest="verbose",
+                        default=False, help="Verbose printing (show output of test)")
     (options) = parser.parse_args()
     main(options)
 
