8458c898b8910ec92c99d69cd0bbfc3ee311de04
P.C. Shyamshankar <shyam@cs.jhu.edu>
2014-07-19 18:04:27 -0400

    Add renicing to k3 process run, fix binary chmod, use pkill.

--- scripts/deployment/classes/k3peer.py
+++ scripts/deployment/classes/k3peer.py
@@ -8,56 +8,57 @@ class K3Peer:
     self.ip = config["ip"]
     self.port = config["k3_port"]
     self.remote_binary_path = "/temp/"
-    self.binary_name = "k3_" + str(self.ip) + str(self.port)  
+    self.binary_name = "k3_" + str(self.ip) + str(self.port)
     self.remote_script_path = "/temp/" + self.binary_name + ".txt"
     self.k3_bindings = config.get("k3_bindings",{})
     self.loggingEnabled = bool(config.get("enable_logging", False))
     # Constants
     self.local_script_path =  "%s.txt" % self.binary_name
     self.host = "root@%s" % self.ip
-  
+
   def deployBinary(self):
     # Kill any running instance of the binary to unlock all files
     local_kill_file = "kill_" + self.binary_name + ".txt"
-    remote_kill_file = self.remote_binary_path + "kill_" + self.binary_name;    
+    remote_kill_file = self.remote_binary_path + "kill_" + self.binary_name;
     script = ("""#!/bin/bash\n"""
-              """kill -9 $(pidof %(bn)s)\n""" % {"bn": self.binary_name })
+              """pkill %s\n""" % self.binary_name )
 
 
     # Dump to a local file
     with open(local_kill_file,"wb") as script_file:
       script_file.write(script)
-  
-    # copy local to remote 
+
+    # copy local to remote
     scpTo(self.host, local_kill_file, remote_kill_file)
- 
+
     # chmod +x remote_script
     chmod_cmd = "chmod +x %s" % remote_kill_file
     ssh(self.host, chmod_cmd)
 
     # call kill command
     ssh(self.host, remote_kill_file)
-    
+
     # give the poor process time to die in peace (and unlock the script file)
-    time.sleep(3) 
-  
+    time.sleep(3)
+
     # copy run script over to remote
     remote_file = self.remote_binary_path + self.binary_name
-      
+
     scpTo(self.host, self.local_binary_path, remote_file) # TODO use rsync
+    ssh(self.host, "chmod +x %s" % remote_file)
 
   def generateScript(self):
     k3_bindings = self.k3_bindings
     self.k3_bindings_str = ("'" + (",".join([k + ":" + v for (k,v) in k3_bindings.items()]))  + "'")
-  
+
     logStr = ""
     if self.loggingEnabled:
       logStr = "-l some"
-    #perf record -g -o /temp/perf/%(bn)s.data 
+    #perf record -g -o /temp/perf/%(bn)s.data
     # Generate a script to run program.
     script = ("""#!/bin/bash\n"""
-              """%(bp)s%(bn)s %(ls)s -p %(bs)s\n""") % {"bn":self.binary_name,"bp":self.remote_binary_path, "bs":self.k3_bindings_str, "ls": logStr}
-    
+              """nice -n 19 %(bp)s%(bn)s %(ls)s -p %(bs)s\n""") % {"bn":self.binary_name,"bp":self.remote_binary_path, "bs":self.k3_bindings_str, "ls": logStr}
+
     # Dump to a local file
     with open(self.local_script_path,"wb") as script_file:
       script_file.write(script)
