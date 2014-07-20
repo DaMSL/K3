dcadd46b4de388592a8fee6f2d8a94a6b6d59181
jwheels <joshdub223@gmail.com>
2014-07-20 00:07:20 -0400

    128 Peer configs with generation script

---
+++ scripts/deployment/gen_config.py
@@ -0,0 +1,60 @@
+import sys
+
+# Constants
+addr_prefix = "192.168.1."
+uv_prefix = "/local/sf5/128l/uservisits_sf5_pp_128l/uservisits_sf5_pp_128l_"
+rankings_prefix = "/local/sf5/128l/rankings_sf5_128l/rankings_sf5_128l_"
+crawl_prefix = "/local/sf5/128l/crawl_sf5_128l/crawl_sf5_128l_"
+
+# Configuration
+num_files = 128
+num_machines = 8
+peers_per_machine = 16
+start_addr = 34
+start_port = 40000
+
+# Script
+
+def gen_peers(handle):
+  curr_addr = start_addr
+  curr_port = start_port
+  curr_file = 0
+  handle.write("\n\n")
+  handle.write("k3_peers:\n")
+  for i in range(num_machines):
+    for j in range(peers_per_machine):
+      addr = addr_prefix + str(curr_addr)
+      port = str(curr_port)
+      uv_file = uv_prefix + "%03d" % curr_file
+      rankings_file = rankings_prefix + "%03d" % curr_file
+      crawl_file = crawl_prefix + "%03d" % curr_file
+
+
+
+      ip_line   = '  - ip: "%s"' % addr
+      port_line = '    k3_port: "%s"' % port
+      uv_line   = """      user_visits_file: '"%s"'""" % uv_file
+      rankings_line   = """      rankings_file: '"%s"'""" % rankings_file
+      crawl_line   = """      crawl_file: '"%s"'""" % crawl_file
+
+      handle.write(ip_line + "\n")
+      handle.write(port_line + "\n")
+      handle.write(uv_line + "\n")
+      handle.write(rankings_line + "\n")
+      handle.write(crawl_line + "\n")
+      curr_port = curr_port + 1
+      curr_file = curr_file + 1
+
+    curr_addr = curr_addr + 1
+
+if __name__ == "__main__":
+  if len(sys.argv) < 3:
+    print("usage %s template_file out_file" % sys.argv[0])
+
+  template = sys.argv[1]
+  out_path = sys.argv[2]
+
+  with open(template, "r") as in_f:
+    with open(out_path, "w") as out_f:
+      out_f.write(in_f.read())
+      gen_peers(out_f)
--- scripts/preprocessing/user_visit_dates.py
+++ scripts/preprocessing/user_visit_dates.py
@@ -4,12 +4,17 @@ import csv
 import sys
 import time
 
+def convert(txt):
+  t = time.strptime(txt,"%Y-%M-%d")
+  return str(calendar.timegm(t))
+
+
+
 def main():
     with open(sys.argv[1]) as input_file:
         reader = csv.reader(input_file)
         for row in reader:
-            t = time.strptime(row[2], "%Y-%M-%d")
-            row[2] = str(calendar.timegm(t))
+            row[2] = convert(row[2])
             print(','.join(row))
 
 if __name__ == '__main__':
