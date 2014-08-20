115c9343e4aa6bf39dd2038a53a32fb5695fce2b
P.C. Shyamshankar <shyam@cs.jhu.edu>
2014-08-20 19:38:33 -0400

    Add more syntactic construct lexing.

--- tools/pygments_k3/pygments_k3/lexer.py
+++ tools/pygments_k3/pygments_k3/lexer.py
@@ -1,4 +1,4 @@
-from pygments.token import Keyword, Text, Token
+from pygments.token import Comment, Keyword, Literal, Operator, Text
 from pygments.lexer import RegexLexer
 
 class K3Lexer(RegexLexer):
@@ -17,16 +17,23 @@ class K3Lexer(RegexLexer):
 
         "expression": [
             "if", "then", "else", "let", "in", "case", "of", "bind", "as", "some", "none",
+            "true", "false"
         ]
     }
 
     tokens = {
         'root': [
-            (r'//.*?$', Token.Comment),
+            (r'//.*?$', Comment),
             (r'\b(' + r'|'.join(keywords["preprocessor"]) + r')\s*\b(?!\.)', Keyword.Namespace),
             (r'\b(' + r'|'.join(keywords["declaration"]) + r')\s*\b(?!\.)', Keyword.Declaration),
             (r'\b(' + r'|'.join(keywords["type"]) + r')\s*\b(?!\.)', Keyword.Type),
             (r'\b(' + r'|'.join(keywords["expression"]) + r')\s*\b(?!\.)', Keyword.Reserved),
-            (r'.*\n', Text),
+
+            (r'[+\-*/<>=!&|\\():;,]+', Operator),
+
+            (r'[0-9]+(\.[0-9]*)?', Literal.Number),
+            (r'"[^"]*"', Literal.String),
+            (r'\b([_a-zA-Z][_a-zA-Z0-9]*)\b', Text),
+            (r'\s+', Text.Whitespace)
         ],
     }
