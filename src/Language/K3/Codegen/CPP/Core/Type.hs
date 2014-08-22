9bdb3cac382696e772d9b98ed04733567870aa13
P.C. Shyamshankar <shyam@cs.jhu.edu>
2014-08-22 12:13:44 -0400

    Add stringification routines for types.

--- src/Language/K3/Codegen/CPP/Core/Type.hs
+++ src/Language/K3/Codegen/CPP/Core/Type.hs
@@ -1,6 +1,9 @@
 module Language.K3.Codegen.CPP.Core.Type where
 
+import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
+
 import Language.K3.Core.Common
+import Language.K3.Codegen.CPP.Core.Stringify
 
 data Type
     = Named Identifier
@@ -10,6 +13,14 @@ data Type
     | Templated Type [Type]
     | Qualified Identifier Type
   deriving (Eq, Read, Show)
+
+instance Stringifiable Type where
+    stringify (Named i) = text i
+    stringify (Inferred) = text "auto"
+    stringify (Parameter i) = text i
+    stringify (Primitive pt) = stringify pt
+    stringify (Templated t ts) = stringify t <> angles (fillSep $ punctuate comma $ map stringify ts)
+    stringify (Qualified i t) = text i <> colon <> colon <> stringify t
 
 data PrimitiveType
     = PBool
