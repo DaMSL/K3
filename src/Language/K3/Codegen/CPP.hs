e16b2407171ec45165517c0d0bd3f8027cf708c3
P.C. Shyamshankar <shyam@cs.jhu.edu>
2014-08-29 11:16:02 -0400

    Fix external CPP interface to stringify program correctly.

--- src/Language/K3/Codegen/CPP.hs
+++ src/Language/K3/Codegen/CPP.hs
@@ -3,10 +3,10 @@
 -- binary.
 module Language.K3.Codegen.CPP (
     module Language.K3.Codegen.CPP.Types,
-    program,
+    stringifyProgram,
     transitionCPPGenS
 ) where
 
 import Language.K3.Codegen.CPP.Types
-import Language.K3.Codegen.CPP.Program(program, transitionCPPGenS)
+import Language.K3.Codegen.CPP.Program (stringifyProgram, transitionCPPGenS)
 
