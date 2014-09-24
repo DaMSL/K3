f3ee63f1bf48f1ba6a522e0b78caa40b5b16615b
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-09-24 16:10:37 -0400

    Add helper accessors for effects.

---
+++ src/Language/K3/Analysis/Effects/Common.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE OverloadedLists #-}
+
+module Language.K3.Analysis.Effects.Common where
+
+import Data.Monoid
+
+import qualified Data.Set as S
+
+import Language.K3.Core.Annotation
+import Language.K3.Analysis.Effects.Core
+
+readSet :: K3 Effect -> S.Set (K3 Symbol)
+readSet (tag -> FRead s) = S.singleton s
+readSet (children -> cs) = mconcat $ map readSet cs
+
+writeSet :: K3 Effect -> S.Set (K3 Symbol)
+writeSet (tag -> FWrite s) = S.singleton s
+writeSet (children -> cs) = mconcat $ map readSet cs
