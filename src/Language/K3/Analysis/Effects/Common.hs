cca514edddd934e624bdade5c5ed21304f6c7c68
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-10-15 14:09:38 -0400

    Simplify equality of symbols by SID.

--- src/Language/K3/Analysis/Effects/Common.hs
+++ src/Language/K3/Analysis/Effects/Common.hs
@@ -17,6 +17,8 @@ import qualified Data.Set as S
 import Language.K3.Core.Annotation
 import Language.K3.Analysis.Effects.Core
 
+(===) :: K3 Symbol -> K3 Symbol -> Bool
+s === k = (fromJust (k @~ isSID) == fromJust (s @~ isSID))
 readSet :: K3 Effect -> S.Set (K3 Symbol)
 readSet (tag -> FRead s) = S.singleton s
 readSet (children -> cs) = mconcat $ map readSet cs
@@ -29,12 +31,12 @@ anySuperStructure :: K3 Symbol -> S.Set (K3 Symbol)
 anySuperStructure s = S.insert s (S.unions $ map anySuperStructure (children s))
 
 hasRead :: K3 Symbol -> K3 Effect -> Bool
-hasRead s (tag -> FRead k) = fromJust (k @~ isSID) == fromJust (s @~ isSID)
+hasRead s (tag -> FRead k) = s === k
 hasRead _ (children -> []) = False
 hasRead s (children -> cs) = any (hasRead s) cs
 
 hasWrite :: K3 Symbol -> K3 Effect -> Bool
-hasWrite s (tag -> FWrite k) = fromJust (k @~ isSID) == fromJust (s @~ isSID)
+hasWrite s (tag -> FWrite k) = s === k
 hasWrite _ (children -> []) = False
 hasWrite s (children -> cs) = any (hasWrite s) cs
 
@@ -48,5 +50,5 @@ hasWriteInFunction s (tag &&& children -> (Symbol _ (PLambda _ f), [])) = hasWri
 hasWriteInFunction s (tag &&& children -> (Symbol _ (PLambda _ f), cs))
     = hasWrite s f || any (hasWriteInFunction s) cs
 hasWriteInFunction s (tag &&& children -> ((Symbol _ PApply), [f, k]))
-    = hasWriteInFunction s f || (fromJust (k @~ isSID) == fromJust (s @~ isSID) && argHasWrite f)
+    = hasWriteInFunction s f || (s === k && hasWriteInFunction k f)
 hasWriteInFunction _ _ = False
