9dab2c38dd584e744fc6aac0b2230deb6de76a1b
Yotam Barnoy <yotambarnoy@gmail.com>
2014-10-17 17:52:25 +0000

    runtime: added serialization for shared_ptr
    We serialize by getting the pointer out of the shared_ptr. This means
    that no notion of sharing is kept -- if you serialize a shared_ptr once,
    and then serialize the same shared_ptr again, it'll create 2 different
    structures when deserialized. This should be fine for our purposes (at
    least for now). There's no way to get at the sharing information, even
    if we convert to boost::shared_ptr locally, so the only way to keep sharing info
    (if we want it) would be by using boost::shared_ptr instead of std::shared_ptr

--- runtime/cpp/Serialization.hpp
+++ runtime/cpp/Serialization.hpp
@@ -89,8 +89,35 @@ void load(archive& ar, asio::ip::address& ip, unsigned int version) {
   ip.from_string(s);
 }
 
+// For std::shared_ptr
+template<class archive, class T>
+void save(archive& ar, const std::shared_ptr<T>& sp, unsigned int version) {
+  T* p = sp.get();  
+  ar << p;
+}
+
+template<class archive, class T>
+void load(archive& ar, std::shared_ptr<T>& sp, unsigned int version) {
+  T* p;
+  ar >> p;
+  sp = std::shared_ptr<T>(p);
+}
+
 }} // namespace boost/serialization
 
+// For address, this does the trick
 BOOST_SERIALIZATION_SPLIT_FREE(boost::asio::ip::address);
+
+namespace boost { namespace serialization {
+// For std::shared_ptr, we need to be more explicit
+template<class Archive, class T>
+inline void serialize(
+    Archive & ar,
+    std::shared_ptr<T>& t,
+    const unsigned int file_version
+){
+    split_free(ar, t, file_version); 
+}
+}} // namespaces
 
 #endif // K3_RUNTIME_SERIALIZATION_H
