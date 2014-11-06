12bc01da7626bed3bcd1b196aac75d0ec08ab3bb
Josh Wheeler <joshdub223@gmail.com>
2014-11-06 05:41:36 +0000

    removed debug and no-opt from compile.sh

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 # Since it's a little tricky to come up with the full command line, you can just use this
 # You might need to adjust it to suit your system
 export SNDPATH=`find .cabal-sandbox -name "*packages.conf.d"`
-dist/build/k3/k3 -I ../K3-Core/lib/k3 -I ../K3-Core/examples/sql compile --gcc -l cpp --cpp-flags='-g -DBOOST_LOG_DYN_LINK -I../K3-Core/runtime/cpp/external -I../K3-Core/runtime/cpp -lboost_serialization -lboost_system -lboost_regex -lboost_thread -lre2 -lpthread -lboost_log_setup -lboost_log -lboost_program_options -ftemplate-depth-1024 -O0' -r ../K3-Core/runtime $@ --mpargs package-db=$SNDPATH --mpsearch ../K3-Core/src
+dist/build/k3/k3 -I ../K3-Core/lib/k3 -I ../K3-Core/examples/sql compile --gcc -l cpp --cpp-flags='-DBOOST_LOG_DYN_LINK -I../K3-Core/runtime/cpp/external -I../K3-Core/runtime/cpp -lboost_serialization -lboost_system -lboost_regex -lboost_thread -lre2 -lpthread -lboost_log_setup -lboost_log -lboost_program_options -ftemplate-depth-1024 -O4' -r ../K3-Core/runtime $@ --mpargs package-db=$SNDPATH --mpsearch ../K3-Core/src
