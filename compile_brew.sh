0d94df49058c860844a2445dc444b0d3165a539c
Yotam Barnoy <yotambarnoy@gmail.com>
2014-11-12 18:03:47 -0500

    Added compilation script for OSX with homebrew

---
+++ compile_brew.sh
@@ -0,0 +1,3 @@
+# Command for compiling cpp file in k3 on OSX with homebrew
+export SNDPATH=`find .cabal-sandbox -name "*packages.conf.d"`
+dist/build/k3/k3 -I ../K3-Core/lib/k3 -I ../K3-Core/examples/sql compile --gcc -l cpp --cpp-flags='-DBOOST_LOG_DYN_LINK -I../K3-Core/runtime/cpp/external -I../K3-Core/runtime/cpp -lboost_serialization-mt -lboost_system-mt -lboost_regex-mt -lboost_thread-mt -lre2 -lpthread -lboost_log_setup-mt -lboost_log-mt -lboost_program_options-mt -ftemplate-depth-1024 -O4' -r ../K3-Core/runtime $@ --mpargs package-db=$SNDPATH --mpsearch ../K3-Core/src
