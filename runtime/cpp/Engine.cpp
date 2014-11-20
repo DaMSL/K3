86d7a37593a91772c26435262b90519eb0bc3ff9
jwheels <joshdub223@gmail.com>
2014-11-20 10:31:03 -0500

    Updates to Json logging
    - Handle multiple peers
    - Add timestamp, peer_id, trigname, message_contents to entries

--- runtime/cpp/Engine.cpp
+++ runtime/cpp/Engine.cpp
@@ -14,9 +14,11 @@ namespace K3 {
       list<Address> processAddrs = deployedNodes(sys_env);
       Address initialAddress;
 
-      // TODO log file as parameter
       if (log_enabled) {
-        log_stream = std::ofstream("k3_log.dsv");
+        for (const auto& addr : processAddrs) {
+          auto s = addressAsString(addr);
+          log_streams[addr] = make_shared<std::ofstream>(s);
+        }
       }
 
       if (!processAddrs.empty()) {
@@ -169,20 +171,6 @@ namespace K3 {
           }
        }
       
-       // JSON Log
-       if (log_enabled) {
-          std::map<std::string, std::string> env = mp->json_bindings(next_message->address());
-          auto s = env.size();
-          int i = 0;          
-          for (const auto& tup : env) {
-            log_stream << tup.second;
-            if (i < s-1) {
-              log_stream << "|";
-            }
-            i++;
-          }
-          log_stream << std::endl;
-       }
 
         return res;
 
@@ -197,33 +185,6 @@ namespace K3 {
       MPStatus curr_status = init_st;
       MPStatus next_status;
       logAt(trivial::trace, "Starting the Message Processing Loop");
-      if(log_enabled) {
-          //TODO handle multiple peers! single file per peer probably easiest
-          std::map<std::string, std::string> env = mp->json_bindings(deployedNodes(*deployment).front());
-          auto s = env.size();
-          int i = 0;          
-          // Header 
-          for (const auto& tup : env) {
-            log_stream << tup.first;
-            if (i < s-1) {
-              log_stream << "|";
-            }
-            i++;
-          }
-          log_stream << std::endl;
-
-          i = 0;
-          // Initial Values 
-          for (const auto& tup : env) {
-            log_stream << tup.second;
-            if (i < s-1) {
-              log_stream << "|";
-            }
-            i++;
-          }
-          log_stream << std::endl;
-      }
-      
 
       while(true) {
         switch (curr_status) {
