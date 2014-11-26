34df6ff3f6de73b2a8a1d918bc178d387a98818c
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-11-26 00:32:45 -0500

    Set function's copy bit based on argument pass.

--- src/Language/K3/Transform/LambdaForms.hs
+++ src/Language/K3/Transform/LambdaForms.hs
@@ -100,7 +100,7 @@ lambdaFormOptE e@(Node (ELambda x :@: as) [b]) = do
   ds <- downstreams
   conf <- transformConfig
   let fs = mapMaybe getEffects ds
-  let (ESymbol (eS env -> tnc -> (symProv -> PLambda (eE env -> fc@(Node (FScope bindings@(binding:closure) :@: _) bes)), returnSymbols))) = fromJust $ e @~ isESymbol
+  let (ESymbol fsym@(eS env -> tnc -> (symProv -> PLambda (eE env -> fc@(Node (FScope bindings@(binding:closure) :@: _) bes)), returnSymbols))) = fromJust $ e @~ isESymbol
   let  moveable (expandSymDeep env -> g) = not (isDerivedFromGlobal env g) &&
                                            not (any (\f -> let SymbolCategories r w _ _ _
                                                                  = effectSCategories f [g] env
@@ -115,6 +115,8 @@ lambdaFormOptE e@(Node (ELambda x :@: as) [b]) = do
           | binding `elemSymbol` cApplied = True
           | binding `elemSymbol` cRead = True
           | otherwise = False
+
+  when funcHint (toggleCopy fsym)
 
   let captHint' (cref, move, copy) s
           | s === binding = return (cref, move, copy)
