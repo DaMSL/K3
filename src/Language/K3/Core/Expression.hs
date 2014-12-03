35ada358cd0a28f4da66500ef53fab6762f78544
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-12-03 14:55:02 -0500

    Add materialization expression annotation to expressions.

--- src/Language/K3/Core/Expression.hs
+++ src/Language/K3/Core/Expression.hs
@@ -40,6 +40,8 @@ import Data.Tree
 import Data.Typeable
 import Data.Word (Word8)
 
+import qualified Data.Map as M
+
 import Language.K3.Core.Annotation
 import Language.K3.Core.Annotation.Analysis
 import Language.K3.Core.Annotation.Codegen
@@ -53,7 +55,7 @@ import Language.K3.Analysis.Provenance.Core
 import qualified Language.K3.Analysis.SEffects.Core as S
 import Language.K3.Analysis.Effects.Core hiding ( Provenance(..) )
 
-import Language.K3.Transform.Hints
+import qualified Language.K3.Codegen.CPP.Materialization as Z
 
 import Language.K3.Utils.Pretty
 
@@ -154,7 +156,7 @@ data instance Annotation Expression
     | EProvenance (K3 Provenance)
     | ESEffect    (K3 S.Effect)
     | EFStructure (K3 S.Effect)
-    | EOpt        OptHint
+    | EMaterialization (M.Map Identifier Z.Decision)
     | EType       (K3 Type)
     | EQType      (K3 QType)
     | ETypeLB     (K3 Type)
