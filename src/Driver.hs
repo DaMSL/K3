d8ab329c41d9ace3ad5de13f6cb6c628e988e04c
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-12-29 15:32:10 +0530

    Remove redundant import.

--- src/Driver.hs
+++ src/Driver.hs
@@ -6,7 +6,6 @@ import Data.Char
 import Data.List(foldl')
 import Data.Maybe
 import Data.Tuple
-import Data.Functor
 
 import qualified Options.Applicative as Options
 import Options.Applicative((<>), (<*>))
