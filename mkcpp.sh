cf20a2cdc0136014235abb28c6cbfd304b4ba1c6
Yanif Ahmad <yanif@jhu.edu>
2015-01-30 19:42:15 -0500

    Minor top-level script changes (adding parse and mkcpp)

--- mkcpp.sh
+++ mkcpp.sh
@@ -5,7 +5,4 @@ export SNDPATH=`find .cabal-sandbox -name "*packages.conf.d"`
 dist/build/k3/k3 \
   -I ../K3-Core/lib/k3 -I ../K3-Core/examples/sql \
   --mpargs package-db=$SNDPATH --mpsearch ../K3-Core/src \
-  compile \
-    --gcc -l cpp \
-    --cpp-flags='-DBOOST_LOG_DYN_LINK -I../K3-Core/runtime/cpp/external -I../K3-Core/runtime/cpp -lboost_serialization -lboost_system -lboost_regex -lboost_thread -lre2 -lyaml-cpp -lpthread -lboost_log_setup -lboost_log -lboost_program_options -ftemplate-depth-1024 -O4' \
-    -r ../K3-Core/runtime $@
+  compile -1 $@ 
