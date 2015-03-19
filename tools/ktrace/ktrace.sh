95d73b8305cc03f6c2a8daa08fd18976590ac636
Josh Wheeler <joshdub223@gmail.com>
2015-03-19 16:16:25 +0000

    Force ktrace to ignore trigger ids

--- tools/ktrace/ktrace.sh
+++ tools/ktrace/ktrace.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [  $# -ne 1 ]
+then
+  echo "Usage: $0 k3_source"
+  exit 1
+fi
+
 # Run from root K3 directory
 export SNDPATH=`find .cabal-sandbox -name "*packages.conf.d"`
 ./dist/build/k3/k3 -I ./lib/k3 -I ./examples/sql compile -l ktrace $@  --mpargs package-db=$SNDPATH --mpsearch ./src
