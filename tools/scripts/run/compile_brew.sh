139860ddfd5efb626a18afbc95ee88e201089856
Josh Wheeler <joshdub223@gmail.com>
2015-03-24 17:48:55 +0000

    Add -D flag to compile_brew script

--- tools/scripts/run/compile_brew.sh
+++ tools/scripts/run/compile_brew.sh
@@ -5,5 +5,5 @@ dist/build/k3/k3 \
 	--mpargs package-db=$SNDPATH --mpsearch src \
 	compile \
 	  --gcc -l cpp \
-		--cpp-flags='-DBOOST_LOG_DYN_LINK -Iruntime/cpp/external -Iruntime/cpp -lboost_serialization-mt -lboost_system-mt -lboost_regex-mt -lboost_thread-mt -lre2 -lyaml-cpp -lpthread -lboost_log_setup-mt -lboost_log-mt -lboost_program_options-mt -lcsvpp -ftemplate-depth-1024 -O4' \
+		--cpp-flags='-DYAS_SERIALIZE_BOOST_TYPES=1 -DBOOST_LOG_DYN_LINK -Iruntime/cpp/external -Iruntime/cpp -lboost_serialization-mt -lboost_system-mt -lboost_regex-mt -lboost_thread-mt -lre2 -lyaml-cpp -lpthread -lboost_log_setup-mt -lboost_log-mt -lboost_program_options-mt -lcsvpp -ftemplate-depth-1024 -O4' \
 		-r runtime $@
