aaa13ec170b19842dc1ddc6058201bfd2de7c35c
root <root@qp6.damsl.cs.jhu.edu>
2015-06-06 23:37:13 +0000

    Directory fix for service script

--- tools/scripts/run/service.sh
+++ tools/scripts/run/service.sh
@@ -4,6 +4,9 @@
 # Since it's a little tricky to come up with the full command line, you can just use this
 # You might need to adjust it to suit your system
 
+export K3HOME=/k3/K3
+cd $K3HOME
+
 export SNDPATH=`find .cabal-sandbox -name "*packages.conf.d"`
 
 # Set a default c++ compiler
@@ -11,6 +14,7 @@ if [ -z "$CXX" ]; then
   CXX="--gcc"
 fi
 
+echo "K3HOME: $K3HOME"
 echo "K3_CXXFLAGS: ${K3_CXXFLAGS}"
 
 dist/build/k3/k3 \
@@ -19,4 +23,4 @@ dist/build/k3/k3 \
   service $1 \
     $CXX -l cpp \
     --cpp-flags="-DBOOST_LOG_DYN_LINK -DYAS_SERIALIZE_BOOST_TYPES=1 -Iruntime/cpp/external -Iruntime/cpp ${K3_CXXFLAGS} -lboost_serialization -lboost_system -lboost_regex -lboost_thread -lre2 -lyaml-cpp -lpthread -lboost_log_setup -lboost_log -lboost_program_options -lcsvpp -ldynamic -ftemplate-depth-1024 -O4" \
-    -r runtime ${@:2}
+    -r runtime ${@:2} 
