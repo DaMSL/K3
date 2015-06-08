8ab948d49a39f5e588c8cc57e96cd392f6b8e383
jwheels <joshdub223@gmail.com>
2015-06-08 10:12:37 -0400

    Add missing Vector file

---
+++ runtime/refactor/include/collections/Vector.hpp
@@ -0,0 +1,202 @@
+#ifndef K3_VECTOR
+#define K3_VECTOR
+
+#include <vector>
+#include "yaml-cpp/yaml.h"
+
+#include "Common.hpp"
+#include "STLDataspace.hpp"
+#include "boost/serialization/vector.hpp"
+#include "boost/serialization/base_object.hpp"
+
+namespace K3 {
+
+template <class Elem>
+class Vector : public STLDS<K3::Vector, std::vector, Elem> {
+  using Super = STLDS<K3::Vector, std::vector, Elem>;
+
+ public:
+  Vector() : Super() {}
+  Vector(const Super& c) : Super(c) {}
+  Vector(Super&& c) : Super(std::move(c)) {}
+
+  // TODO bounds checking
+  Elem at(int i) const {
+    auto& vec = Super::getConstContainer();
+    return vec[i];
+  }
+
+  // TODO bounds checking
+  unit_t set(int i, Elem f) {
+    auto& vec = Super::getContainer();
+    vec[i] = f;
+    return unit_t();
+  }
+
+  unit_t inPlaceAdd(const Vector<Elem>& other) {
+    auto& vec = Super::getContainer();
+    auto& other_vec = other.getConstContainer();
+    if (vec.size() != other_vec.size()) {
+      throw std::runtime_error("Vector inPlaceAdd size mismatch");
+    }
+
+    #pragma clang loop vectorize(enable) interleave(enable)
+    for (int i = 0; i < vec.size(); i++) {
+      vec[i].elem += other_vec[i].elem;
+    }
+
+    return unit_t{};
+  }
+
+  Vector<Elem> add(const Vector<Elem>& other) const {
+    auto copy = Vector<Elem>(*this);
+    copy.inPlaceAdd(other);
+    return copy;
+  }
+
+  Vector<Elem> add(Vector<Elem>&& other) const {
+    other.inPlaceAdd(*this);
+    return other;
+  }
+
+  unit_t inPlaceSub(const Vector<Elem>& other) {
+    auto& vec = Super::getContainer();
+    auto& other_vec = other.getConstContainer();
+    if (vec.size() != other_vec.size()) {
+      throw std::runtime_error("Vector inPlaceSub size mismatch");
+    }
+
+    #pragma clang loop vectorize(enable) interleave(enable)
+    for (int i = 0; i < vec.size(); i++) {
+      vec[i].elem -= other_vec[i].elem;
+    }
+
+    return unit_t{};
+  }
+
+  unit_t inPlaceSubFrom(const Vector<Elem>& other) {
+    auto& vec = Super::getContainer();
+    auto& other_vec = other.getConstContainer();
+    if (vec.size() != other_vec.size()) {
+      throw std::runtime_error("Vector inPlaceSub size mismatch");
+    }
+
+    #pragma clang loop vectorize(enable) interleave(enable)
+    for (int i = 0; i < vec.size(); i++) {
+      vec[i].elem = other_vec[i].elem - vec[i].elem;
+    }
+
+    return unit_t{};
+  }
+
+  Vector<Elem> sub(const Vector<Elem>& other) const {
+    auto copy = Vector<Elem>(*this);
+    copy.inPlaceSub(other);
+    return copy;
+  }
+
+  Vector<Elem> sub(Vector<Elem>&& other) const {
+    other.inPlaceSubFrom(*this);
+    return other;
+  }
+
+  double dot(const Vector<Elem>& other) const {
+    auto& vec = Super::getConstContainer();
+    auto& other_vec = other.getConstContainer();
+    if (vec.size() != other_vec.size()) {
+      throw std::runtime_error("Vector dot size mismatch");
+    }
+
+    double d = 0;
+
+    #pragma clang loop vectorize(enable) interleave(enable)
+    for (int i = 0; i < vec.size(); i++) {
+      d += vec[i].elem * other_vec[i].elem;
+    }
+    return d;
+  }
+
+  double distance(const Vector<Elem>& other) const {
+    double d = 0;
+    auto& vec = Super::getConstContainer();
+    auto& other_vec = other.getConstContainer();
+    if (vec.size() != other_vec.size()) {
+      throw std::runtime_error("Vector distance size mismatch");
+    }
+
+    #pragma clang loop vectorize(enable) interleave(enable)
+    for (int i = 0; i < vec.size(); i++) {
+      d += pow(vec[i].elem - other_vec[i].elem, 2);
+    }
+
+    return sqrt(d);
+  }
+
+  string toString(unit_t) const {
+    std::ostringstream oss;
+    auto& c = Super::getConstContainer();
+    int i = 0;
+    oss << "[";
+    for (auto& relem : c) {
+      oss << relem.elem;
+      i++;
+      if (i < c.size()) {
+        oss << ",";
+      }
+    }
+    oss << "]";
+    return oss.str();
+  }
+
+  // PERF: Vectorize.
+  unit_t inPlaceScalarMult(double c) {
+    #pragma clang loop vectorize(enable) interleave(enable)
+    for (auto& i : this->getContainer()) {
+      i.elem *= c;
+    }
+  }
+
+  Vector<Elem> scalarMult(double c) const {
+    auto result = Vector<Elem>(*this);
+    auto& vec = result.getContainer();
+
+   #pragma clang loop vectorize(enable) interleave(enable)
+    for (auto i = 0; i < vec.size(); ++i) {
+      vec[i].elem *= c;
+    }
+
+    return result;
+  }
+};
+
+}  // namespace K3
+
+namespace YAML {
+
+template <class E>
+struct convert<K3::Vector<E>> {
+  static Node encode(const K3::Vector<E>& c) {
+    Node node;
+    auto container = c.getConstContainer();
+    if (container.size() > 0) {
+      for (auto i : container) {
+        node.push_back(convert<E>::encode(i));
+      }
+    } else {
+      node = YAML::Load("[]");
+    }
+    return node;
+  }
+
+  static bool decode(const Node& node, K3::Vector<E>& c) {
+    for (auto& i : node) {
+      c.insert(i.as<E>());
+    }
+
+    return true;
+  }
+};
+
+}  // namespace YAML
+
+#endif
