37e8854e442dfd54677da9011d0cfe1619e51182
Yanif Ahmad <yanif@jhu.edu>
2015-07-08 03:35:11 -0400

    Fixes for correct composite_key return type on mi-vmaps

--- src/Language/K3/Codegen/CPP/MultiIndex.hs
+++ src/Language/K3/Codegen/CPP/MultiIndex.hs
@@ -32,9 +32,9 @@ import Language.K3.Codegen.CPP.Types
 -- ii. function definitions to attach as members for this annotation combination (lookup functions)
 indexes ::  Identifier -> [(Identifier, [AnnMemDecl])] -> [K3 Type] -> CPPGenM ([R.Type], [R.Definition])
 indexes name ans content_ts = do
-  let indexed   = zip [1..] ans
-  let flattened = filter is_index_mem $ concatMap (\(n, (i, mems)) -> zip (repeat (n,i)) mems) indexed
-  index_types       <- (nub . catMaybes) <$> mapM index_type flattened
+  let indexed   = zip [1..] $ filter is_index_mem ans
+  let flattened = concatMap (\(i, (n, mems)) -> zip (repeat (i,n)) mems) indexed
+  index_types       <- (map snd . nub . catMaybes) <$> mapM index_type flattened
   lookup_defns      <- catMaybes <$> mapM lookup_fn flattened
   lookup_with_defns <- catMaybes <$> mapM lookup_with_fn flattened
   slice_defns       <- catMaybes <$> mapM slice_fn flattened
@@ -49,7 +49,7 @@ indexes name ans content_ts = do
     elem_r =  R.Name "&__CONTENT"
     bmi n = R.Qualified (R.Name "boost") (R.Qualified (R.Name "multi_index") n)
 
-    is_index_mem ((_,n),_) = not ("MultiIndex" `isInfixOf` n) && ("Index" `isInfixOf` n)
+    is_index_mem (n,_) = not ("MultiIndex" `isInfixOf` n) && ("Index" `isInfixOf` n)
 
     get_key_type :: Identifier -> K3 Type -> Maybe (K3 Type)
     get_key_type n (PTFunction k f _)
@@ -60,13 +60,12 @@ indexes name ans content_ts = do
 
     get_key_type _ _ = Nothing
 
-    index_type :: ((Integer, Identifier), AnnMemDecl) -> CPPGenM (Maybe R.Type)
-    index_type ((_,n), decl) =
-      if "Index" `isInfixOf` n then extract_type n decl else return Nothing
+    index_type :: ((Integer, Identifier), AnnMemDecl) -> CPPGenM (Maybe (Integer, R.Type))
+    index_type ((i,n), decl) = extract_type i n decl
 
     -- Build a boost index type e.g. ordered_non_unique
-    extract_type :: Identifier -> AnnMemDecl -> CPPGenM (Maybe R.Type)
-    extract_type n (Lifted _ _ t _ memanns) = do
+    extract_type :: Integer -> Identifier -> AnnMemDecl -> CPPGenM (Maybe (Integer, R.Type))
+    extract_type i n (Lifted _ _ t _ memanns) = do
       let key_t = get_key_type n t
       let fields = maybe Nothing get_fields key_t
       let idx_t = if "Ordered" `isInfixOf` n then "ordered_non_unique" else "hashed_non_unique"
@@ -77,13 +76,13 @@ indexes name ans content_ts = do
                  (rkt, rcovt) <- get_extractor_root_types n (head content_ts) elem_type
                  nxts <- get_extractors rkt elem_type rcovt memanns
                  extractor_types <- mapM (field_extractor_type nxts) fnt
-                 i_t <- specialize_index_type idx_t extractor_types
-                 return $ Just i_t
+                 i_t <- specialize_index_type n idx_t extractor_types
+                 return $ Just (i, i_t)
                _ -> return Nothing
 
-    extract_type _ _ = return Nothing
+    extract_type _ _ _ = return Nothing
 
-    index_content_err :: Int -> CPPGenM (Maybe R.Type)
+    index_content_err :: Int -> CPPGenM (Maybe (Integer, R.Type))
     index_content_err i = case i of
         0 -> throwE $ CPPGenE "No content type found while constructing multi-index"
         j | j >= 2 -> throwE $ CPPGenE "Multiple content types found while constructing multi-index"
@@ -177,14 +176,17 @@ indexes name ans content_ts = do
     member_extractor e_t c_t e_path =
       R.Named $ R.Specialized [e_t, c_t, e_path] (bmi $ R.Name "member")
 
-    specialize_index_type :: Identifier -> [R.Type] -> CPPGenM R.Type
-    specialize_index_type _ [] = throwE $ CPPGenE "Empty index members"
-    specialize_index_type idx_t [x_t] =
+    specialize_index_type :: Identifier -> Identifier -> [R.Type] -> CPPGenM R.Type
+    specialize_index_type _ _ [] = throwE $ CPPGenE "Empty index members"
+    specialize_index_type _ idx_t [x_t] =
       return $ R.Named $ R.Specialized [x_t] (bmi $ R.Name idx_t)
 
-    specialize_index_type idx_t extractor_ts =
+    specialize_index_type n idx_t extractor_ts = do
+      let comp_ret_t = if "VMap" `isInfixOf` n
+                         then (R.Named $ R.Specialized [elem_type, R.Primitive R.PInt] $ R.Name "K3::VElem")
+                         else elem_type
       return $ R.Named $ R.Specialized
-        [ R.Named $ R.Specialized (elem_type : extractor_ts) (bmi $ R.Name "composite_key") ]
+        [ R.Named $ R.Specialized (comp_ret_t : extractor_ts) (bmi $ R.Name "composite_key") ]
         (bmi $ R.Name idx_t)
 
     tuple :: R.Name -> K3 Type -> R.Expression
