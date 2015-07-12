705bb968fb5b34bfc1a3b5c22b85288ce8caf8cf
P.C. Shyamshankar <sykora@lucentbeing.com>
2015-07-12 16:34:58 -0400

    Remove superfluous move of shared_ptr in dispatcher.

--- src/Language/K3/Codegen/CPP/Expression.hs
+++ src/Language/K3/Codegen/CPP/Expression.hs
@@ -332,7 +332,7 @@ inline e@(tag &&& children -> (EOperate OSnd, [tag &&& children -> (ETuple, [tri
     return (concat [te, ae, ve]
                  ++ [ classInst
                     , R.Ignore $ R.Call (R.Project (R.Variable $ R.Name "__engine") (R.Name "send")) [
-                                    av, R.Variable (R.Name $ tIdName), R.Move (R.Variable $ R.Name d), R.Variable (R.Name "me")
+                                    av, R.Variable (R.Name $ tIdName), R.Variable $ R.Name d, R.Variable (R.Name "me")
                                    ]
                     ]
              , R.Initialization R.Unit [])
