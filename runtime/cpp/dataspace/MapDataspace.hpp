6b7835faf9e5beff669c33f0adb1fee7fe04c5e1
Yotam Barnoy <yotambarnoy@gmail.com>
2015-07-14 14:21:57 -0400

    mapdataspace: added sortedMapE

--- runtime/cpp/dataspace/MapDataspace.hpp
+++ runtime/cpp/dataspace/MapDataspace.hpp
@@ -2333,6 +2333,480 @@ class SortedMap {
   friend class boost::serialization::access;
 }; // class SortedMap
 
+template<class R>
+class SortedMapE {
+  using Key = typename R::KeyType;
+  using Value = typename R::ValueType;
+  using Container = std::map<Key, R>;
+
+ public:
+  // Default Constructor
+  SortedMapE(): container() {}
+  SortedMapE(const std::map<Key,R>& con): container(con) {}
+  SortedMapE(std::map<Key, R>&& con): container(std::move(con)) {}
+
+  // Construct from (container) iterators
+  template<typename Iterator>
+  SortedMapE(Iterator begin, Iterator end): container(begin,end) {}
+
+  template <class Pair>
+  R elemToRecord(const Pair& e) const { return e.second; }
+
+  template <class I>
+  class map_iterator: public std::iterator<std::forward_iterator_tag, R> {
+    using container = std::map<Key, R>;
+    using reference = typename std::iterator<std::forward_iterator_tag, R>::reference;
+   public:
+    template <class _I>
+    map_iterator(_I&& _i): i(std::forward<_I>(_i)) {}
+
+    map_iterator& operator ++() {
+      ++i;
+      return *this;
+    }
+
+    map_iterator operator ++(int) {
+      map_iterator t = *this;
+      *this++;
+      return t;
+    }
+
+    auto operator->() const {
+      return &(i->second);
+    }
+
+    auto& operator*() const {
+      return i->second;
+    }
+
+    bool operator ==(const map_iterator& other) const {
+      return i == other.i;
+    }
+
+    bool operator !=(const map_iterator& other) const {
+      return i != other.i;
+    }
+
+   private:
+    I i;
+  };
+
+  using iterator = map_iterator<typename std::map<Key, R>::iterator>;
+  using const_iterator = map_iterator<typename std::map<Key, R>::const_iterator>;
+
+  iterator begin() {
+    return iterator(container.begin());
+  }
+
+  iterator end() {
+    return iterator(container.end());
+  }
+
+  const_iterator begin() const {
+    return const_iterator(container.cbegin());
+  }
+
+  const_iterator end() const {
+    return const_iterator(container.cend());
+  }
+
+  shared_ptr<R> peek(const unit_t&) const {
+    shared_ptr<R> res(nullptr);
+    auto it = container.begin();
+    if (it != container.end()) {
+      res = std::make_shared<R>(it->second);
+    }
+    return res;
+  }
+
+  template<typename F, typename G>
+  auto peek_with(F f, G g) const {
+    auto it = container.begin();
+    if (it == container.end()) {
+      return f(unit_t {});
+    } else {
+      return g(it->second);
+    }
+  }
+
+  template <class Q>
+  unit_t insert(Q&& q) {
+    container[q.key] = std::forward<Q>(q);
+    return unit_t();
+  }
+
+  template <class F>
+  unit_t insert_with(const R& rec, F f) {
+    auto existing = container.find(rec.key);
+    if (existing == std::end(container)) {
+      container[rec.key] = rec;
+    } else {
+      container[rec.key] = f(std::move(existing->second))(rec);
+    }
+
+    return unit_t {};
+  }
+
+  template <class F, class G>
+  unit_t upsert_with(const R& rec, F f, G g) {
+    auto existing = container.find(rec.key);
+    if (existing == std::end(container)) {
+      container[rec.key] = f(unit_t {});
+    } else {
+      container[rec.key] = g(std::move(existing->second));
+    }
+
+    return unit_t {};
+  }
+
+  unit_t erase(const R& rec) {
+    auto it = container.find(rec.key);
+    if (it != container.end()) {
+        container.erase(it);
+    }
+    return unit_t();
+  }
+
+  unit_t update(const R& rec1, const R& rec2) {
+    auto it = container.find(rec1.key);
+    if (it != container.end()) {
+        container.erase(it);
+        container[rec2.key] = rec2;
+    }
+    return unit_t();
+  }
+
+  int size(unit_t) const { return container.size(); }
+
+  SortedMapE combine(const SortedMapE& other) const {
+    // copy this DS
+    SortedMapE result = SortedMapE(*this);
+    // copy other DS
+    for (const auto& p: other.container) {
+      result.container[p.first] = p.second;
+    }
+    return result;
+  }
+
+  tuple<SortedMapE, SortedMapE> split(const unit_t&) const {
+    // Find midpoint
+    const size_t size = container.size();
+    const size_t half = size / 2;
+    // Setup iterators
+    auto beg = container.begin();
+    auto mid = container.begin();
+    std::advance(mid, half);
+    auto end = container.end();
+    // Construct DS from iterators
+    return std::make_tuple(SortedMapE(beg, mid), SortedMapE(mid, end));
+  }
+
+  template <typename Fun>
+  unit_t iterate(Fun f) const {
+    for (const auto& p : container) {
+      f(p.second);
+    }
+    return unit_t();
+  }
+
+  template<typename Fun>
+  auto map(Fun f) const -> SortedMapE< RT<Fun, R> > {
+    SortedMapE< RT<Fun,R> > result;
+    for (const auto& p : container) {
+      result.insert( f(p.second) );
+    }
+    return result;
+  }
+
+  template <typename Fun>
+  SortedMapE<R> filter(Fun predicate) const {
+    SortedMapE<R> result;
+    for (const auto& p : container) {
+      if (predicate(p.second)) {
+        result.insert(p.second);
+      }
+    }
+    return result;
+  }
+
+  template<typename Fun, typename Acc>
+  Acc fold(Fun f, Acc acc) const {
+    for (const auto& p : container) {
+      acc = f(std::move(acc))(p.second);
+    }
+    return acc;
+  }
+
+  template<typename F1, typename F2, typename Z>
+  SortedMapE< R_key_value< RT<F1, R>,Z >> groupBy(F1 grouper, F2 folder, const Z& init) const {
+    // Create a map to hold partial results
+    typedef RT<F1, R> K;
+    std::map<K, Z> accs;
+
+    for (const auto& it : container) {
+      K key = grouper(it.second);
+      if (accs.find(key) == accs.end()) {
+        accs[key] = init;
+      }
+      accs[key] = folder(std::move(accs[key]))(it.second);
+    }
+
+    // TODO more efficient implementation?
+    SortedMapE<R_key_value<K,Z>> result;
+    for (auto&& it : accs) {
+      result.insert(std::move(R_key_value<K, Z> {std::move(it.first), std::move(it.second)}));
+    }
+    return result;
+  }
+
+  template <class Fun>
+  auto ext(Fun expand) const -> SortedMapE < typename RT<Fun, R>::ElemType >  {
+    typedef typename RT<Fun, R>::ElemType T;
+    SortedMapE<T> result;
+    for (const auto& it : container) {
+      for (auto& it2 : expand(it.second).container) {
+        result.insert(it2.second);
+      }
+    }
+
+    return result;
+  }
+
+  // Accumulate over a sampled ds.
+  // This number of items accessed depends on the iterator implementation, via std::advance.
+  template<typename Fun, typename Acc>
+  Acc sample(Fun f, Acc acc, size_t sampleSize) const {
+    auto it = container.begin();
+    DSSampler seqSampler(container.size(), sampleSize);
+
+    tuple<bool, size_t> next_skip = seqSampler.next();
+    while ( it != container.end() && std::get<0>(next_skip) ) {
+      std::advance(it, std::get<1>(next_skip));
+      if ( it != container.end() ) {
+        acc = f(std::move(acc))(*it);
+        next_skip = seqSampler.next();
+      }
+    }
+
+    return acc;
+  }
+
+  // lookup ignores the value of the argument
+  shared_ptr<R> lookup(const R& r) const {
+      auto it = container.find(r.key);
+      if (it != container.end()) {
+        return std::make_shared<R>(it->second);
+      } else {
+        return nullptr;
+      }
+  }
+
+  bool member(const R& r) const {
+    return container.find(r.key) != container.end();
+  }
+
+  template <class F>
+  unit_t lookup_with(R const& r, F f) const {
+    auto it = container.find(r.key);
+    if (it != container.end()) {
+      return f(it->second);
+    }
+
+    return unit_t {};
+  }
+
+  template <class F, class G>
+  auto lookup_with2(R const& r, F f, G g) const {
+    auto it = container.find(r.key);
+    if (it == container.end()) {
+      return f(unit_t {});
+    } else {
+      return g(it->second);
+    }
+  }
+
+  template <class F>
+  auto lookup_with3(R const& r, F f) const {
+    auto it = container.find(r.key);
+    if (it != container.end()) {
+      return f(it->second);
+    }
+    throw std::runtime_error("No match on Map.lookup_with3");
+  }
+
+  template <class F, class G>
+  auto lookup_with4(R const& r, F f, G g) const {
+    auto it = container.find(r.key);
+    if (it == container.end()) {
+      return f(unit_t {});
+    } else {
+      return g(it->second);
+    }
+  }
+
+  // Extremal key operations.
+
+  shared_ptr<R> min(const unit_t&) const {
+    shared_ptr<R> res(nullptr);
+    auto it = container.begin();
+    if (it != container.end()) {
+      res = std::make_shared<R>(it->second);
+    }
+    return res;
+  }
+
+  shared_ptr<R> max(const unit_t&) const {
+    shared_ptr<R> res(nullptr);
+    auto it = container.rbegin();
+    if (it != container.rend()) {
+      res = std::make_shared<R>(it->second);
+    }
+    return res;
+  }
+
+  template<typename F, typename G>
+  auto min_with(F f, G g) const {
+    auto it = container.begin();
+    if (it == container.end()) {
+      return f(unit_t {});
+    } else {
+      return g(it->second);
+    }
+  }
+
+  template<typename F, typename G>
+  auto max_with(F f, G g) const {
+    auto it = container.rbegin();
+    if (it == container.rend()) {
+      return f(unit_t {});
+    } else {
+      return g(it->second);
+    }
+  }
+
+  std::shared_ptr<R> lower_bound(const R& rec) const {
+    const auto& x = getConstContainer();
+    auto it = x.lower_bound(rec.key);
+    std::shared_ptr<R> result(nullptr);
+    if (it != x.end()) {
+      result = std::make_shared<R>(it->second);
+    }
+    return result;
+  }
+
+  std::shared_ptr<R> upper_bound(const R& rec) const {
+    const auto& x = getConstContainer();
+    auto it = x.upper_bound(rec.key);
+    std::shared_ptr<R> result(nullptr);
+    if (it != x.end()) {
+      result = std::make_shared<R>(it->second);
+    }
+    return result;
+  }
+
+  template<typename R, typename F, typename G>
+  auto upper_bound_with(const R& rec, F f, G g) const {
+    auto it = container.upper_bound(rec.key);
+    if (it == container.end()) {
+      return f(unit_t {});
+    } else {
+      return g(it->second);
+    }
+  }
+
+  template<typename R, typename F, typename G>
+  auto lower_bound_with(const R& rec, F f, G g) const {
+    auto it = container.lower_bound(rec.key);
+    if (it == container.end()) {
+      return f(unit_t {});
+    } else {
+      return g(it->second);
+    }
+  }
+
+  SortedMapE<R> filter_lt(const R& rec) const {
+    const auto& x = getConstContainer();
+    auto it = x.lower_bound(rec.key);
+    return SortedMapE<R>(x.begin(), it);
+  }
+
+  SortedMapE<R> filter_gt(const R& rec) const {
+    const auto& x = getConstContainer();
+    auto it = x.upper_bound(rec.key);
+    return SortedMapE<R>(it, x.end());
+  }
+
+  SortedMapE<R> filter_geq(const R& rec) const {
+    const auto& x = getConstContainer();
+    auto it = x.lower_bound(rec.key);
+    return SortedMapE<R>(it, x.end());
+  }
+
+  SortedMapE<R> between(const R& a, const R& b) const {
+    const auto& x = getConstContainer();
+    auto it = x.lower_bound(a.key);
+    auto end = x.upper_bound(b.key);
+    if ( it != x.end() ){
+      return SortedMapE<R>(it, end);
+    } else {
+      return SortedMapE<R>();
+    }
+  }
+
+  // Range-based modification, exclusive of the given key.
+
+  unit_t erase_prefix(const R& rec) {
+    auto it = container.lower_bound(rec.key);
+    if (it != container.end()) {
+        container.erase(container.cbegin(), it);
+    }
+    return unit_t();
+  }
+
+  unit_t erase_suffix(const R& rec) {
+    auto it = container.upper_bound(rec.key);
+    if (it != container.end()) {
+        container.erase(it, container.cend());
+    }
+    return unit_t();
+  }
+
+  bool operator==(const SortedMapE& other) const {
+    return container == other.container;
+  }
+
+  bool operator!=(const SortedMapE& other) const {
+    return container != other.container;
+  }
+
+  bool operator<(const SortedMapE& other) const {
+    return container < other.container;
+  }
+
+  bool operator>(const SortedMapE& other) const {
+    return container > other.container;
+  }
+
+  Container& getContainer() { return container; }
+
+  const Container& getConstContainer() const { return container; }
+
+  template<class Archive>
+  void serialize(Archive &ar) { ar & container; }
+
+  template<class Archive>
+  void serialize(Archive &ar, const unsigned int) {
+    ar & boost::serialization::make_nvp("__K3SortedMapE", container);
+  }
+
+ protected:
+  std::map<Key,R> container;
+
+ private:
+  friend class boost::serialization::access;
+}; // class SortedMapE
+
+
 
 template<class R>
 class MapE {
