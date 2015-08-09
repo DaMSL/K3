14c4c1646294cfde85de7dd7f03421c15d78ca9f
P.C. Shyamshankar <sykora@lucentbeing.com>
2015-08-09 16:57:06 -0400

    Add longer logging format for MPred/MExpr, with reasons.

--- src/Language/K3/Codegen/CPP/Materialization/Core.hs
+++ src/Language/K3/Codegen/CPP/Materialization/Core.hs
@@ -3,6 +3,8 @@
 
 module Language.K3.Codegen.CPP.Materialization.Core where
 
+import Data.Tree
+
 import Text.Printf
 
 import Language.K3.Core.Annotation
@@ -18,19 +20,31 @@ data MExpr
   | MIfThenElse (K3 MPred)
  deriving (Eq, Read, Show)
 
-simpleShowE :: K3 MExpr -> String
-simpleShowE m = case tag m of
-  MVar (Juncture u i) d -> printf "%d/%s/%s" (gUID u) i (show d)
-  MAtom t -> show t
-  MIfThenElse p ->
-    let [t, e] = children m
-    in printf "if %s then %s else %s" (simpleShowP p) (simpleShowE t) (simpleShowE e)
-
-data instance Annotation MExpr
+data instance Annotation MExpr = MEReason String
 
 deriving instance Eq (Annotation MExpr)
 deriving instance Read (Annotation MExpr)
 deriving instance Show (Annotation MExpr)
+
+isMEReason :: Annotation MExpr -> Bool
+isMEReason (MEReason _) = True
+isMEReason _ = False
+
+instance Pretty (K3 MExpr) where
+  prettyLines (Node (t :@: as) cs) = case t of
+    MVar (Juncture u i) d -> [printf "MVar %d/%s/%s%s" (gUID u) i (show d) reason]
+    MAtom m -> [printf "MAtom %s%s" (show m) reason]
+    MIfThenElse p -> [printf "MIfThenElse%s" reason] ++ ["|"] ++ (shift "+- " "|  " $ prettyLines p) ++ drawSubTrees cs
+   where
+    reason = maybe "" (\(MEReason s) -> printf " -??- %s" s) (as @~ isMEReason)
+
+ppShortE :: K3 MExpr -> String
+ppShortE m = case tag m of
+  MVar (Juncture u i) d -> printf "%d/%s/%s" (gUID u) i (show d)
+  MAtom t -> show t
+  MIfThenElse p ->
+    let [t, e] = children m
+    in printf "if %s then %s else %s" (ppShortP p) (ppShortE t) (ppShortE e)
 
 data MPred
   = MNot
@@ -40,17 +54,41 @@ data MPred
   | MBool Bool
  deriving (Eq, Read, Show)
 
-simpleShowP :: K3 MPred -> String
-simpleShowP p = case (tag p, children p) of
-  (MNot, [c]) -> printf "-!- %s" (simpleShowP c)
-  (MAnd, [a, b]) -> printf "%s -&&- %s" (simpleShowP a) (simpleShowP b)
-  (MOr, [a, b]) -> printf "%s -||- %s" (simpleShowP a) (simpleShowP b)
-  (MOneOf m ms, _) -> printf "%s ∈ %s" (simpleShowE m) (show ms)
-  (MBool b, _) -> show b
+data instance Annotation MPred = MPReason String
 
-data instance Annotation MPred
 deriving instance Eq (Annotation MPred)
 deriving instance Read (Annotation MPred)
 deriving instance Show (Annotation MPred)
 
+isMPReason :: Annotation MPred -> Bool
+isMPReason (MPReason _) = True
+isMPReason _ = False
+
+instance Pretty (K3 MPred) where
+  prettyLines (Node (t :@: as) cs) = case t of
+    MBool b -> [printf "MBool %s%s" (show b) reason]
+    MOneOf m ms -> [printf "MOneOf %s%s" (show ms) reason] ++ ["|"] ++ shift "`- " "   " (prettyLines m)
+    _ -> [show t ++ reason] ++ drawSubTrees cs
+   where
+    reason = maybe "" (\(MPReason s) -> printf " -??- %s" s) (as @~ isMPReason)
+
+ppShortP :: K3 MPred -> String
+ppShortP p = case (tag p, children p) of
+  (MNot, [c]) -> printf "-!- %s" (ppShortP c)
+  (MAnd, [a, b]) -> printf "%s -&&- %s" (ppShortP a) (ppShortP b)
+  (MOr, [a, b]) -> printf "%s -||- %s" (ppShortP a) (ppShortP b)
+  (MOneOf m ms, _) -> printf "%s ∈ %s" (ppShortE m) (show ms)
+  (MBool b, _) -> show b
+
 data Juncture = Juncture UID Identifier deriving (Eq, Ord, Read, Show)
+
+class Explainable a where
+  (-??-) :: a -> String -> a
+
+instance Explainable (K3 MExpr) where
+  (-??-) e r = e @+ (MEReason r)
+
+instance Explainable (K3 MPred) where
+  (-??-) e r = e @+ (MPReason r)
+
+infixl 9 -??-
