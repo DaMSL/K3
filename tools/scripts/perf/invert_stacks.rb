24ccc53e4e3c7d4bc9a4e6d3aa2a1ae8422bd637
Yotam Barnoy <yotambarnoy@gmail.com>
2015-10-29 14:44:25 -0400

    scripts/invert_stacks: fix and add support for pruning at certain percentages

--- tools/scripts/perf/invert_stacks.rb
+++ tools/scripts/perf/invert_stacks.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
+# Invert stacks
+
+require 'optparse'
 
 def run(f)
   hash = {}
-  top_hash = {}
+  fn_hash = {}
+  total = 0
   File.open(f, "r") do |file|
     while (line = file.gets)
       r = /^(.*) (\d+)$/.match(line)
       num = r[2].to_i
+      total += num
       stack_line = r[1]
       # do inversion
       elems = stack_line.split(";")
@@ -16,25 +21,52 @@ def run(f)
         if hash.has_key?(elems)
           hash[elems] += num
         else
-          hash[elems] = num 
+          hash[elems] = num
         end
         hd = elems[0]
-        if top_hash.has_key?(hd)
-          hash[hd] += num
+        # save functions and their totals
+        if fn_hash.has_key?(hd)
+          fn_hash[hd] += num
         else
-          hash[hd] = num
+          fn_hash[hd] = num
         end
         # remove the head
         elems = elems[1..-1]
       end
     end
   end
+  totalf = total.to_f
   arr = []
+  max_pct = $options[:prune_pct].to_f / 100.0
   hash.each_pair do |k, v|
-    arr << (k.join(";") + " " + v.to_s)
+    # check that we pass the percentage
+    key_num = fn_hash[k[0]]
+    key_pct = key_num / totalf
+    if key_pct <= max_pct then 
+        arr << (k.join(";") + " " + v.to_s)
+    end
   end
   arr.sort!
   arr.each {|s| puts s}
 end
 
-run(ARGV[0])
+def main()
+  $options = {}
+  $options[:prune_pct] = 10 # default
+
+  usage = "Usage: #{$PROGRAM_NAME} stack_file options"
+  parser = OptionParser.new do |opts|
+    opts.banner = usage
+    opts.on("--prune [INT]", "Prune percentage") { |i| $options[:prune_pct] = i }
+  end
+  parser.parse!
+
+  unless ARGV.size == 1
+    puts parser.help
+    exit(1)
+  end
+
+  run(ARGV[0])
+end
+
+main()
