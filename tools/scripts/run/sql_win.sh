f14a6ae3067903aac2a79de6e9e98c9ebb208143
Ben Ring <benring96@gmail.com>
2015-11-12 17:27:03 +0000

    Update compile scripts to use new macros

--- tools/scripts/run/sql_win.sh
+++ tools/scripts/run/sql_win.sh
@@ -15,5 +15,5 @@ dist/build/k3/k3 \
   --mpargs package-db=$SNDPATH --mpsearch src \
   sql \
     --gcc -l cpp \
-    --cpp-flags="-DK3DEBUG -DBOOST_LOG_DYN_LINK -DYAS_SERIALIZE_BOOST_TYPES=1 -D_WIN32_WINNT=0x600 -Iruntime/cpp/include/ -Iruntime/cpp/include/external -L/mingw64/lib -L../extra/lib ${K3_CXXFLAGS} -lboost_serialization-mt -lboost_system-mt -lboost_regex-mt -lboost_thread-mt -lyaml-cpp -lpthread -lboost_log_setup-mt -lboost_log-mt -lboost_program_options-mt -lwsock32 -lws2_32 -lcsvpp -ldynamic -ftemplate-depth-1024 -O4" \
+    --cpp-flags="-DK3MESSAGETRACE -DK3GLOBALTRACE -DK3TRIGGERTIMES -DBOOST_LOG_DYN_LINK -DYAS_SERIALIZE_BOOST_TYPES=1 -D_WIN32_WINNT=0x600 -Iruntime/cpp/include/ -Iruntime/cpp/include/external -L/mingw64/lib -L../extra/lib ${K3_CXXFLAGS} -lboost_serialization-mt -lboost_system-mt -lboost_regex-mt -lboost_thread-mt -lyaml-cpp -lpthread -lboost_log_setup-mt -lboost_log-mt -lboost_program_options-mt -lwsock32 -lws2_32 -lcsvpp -ldynamic -ftemplate-depth-1024 -O4" \
     -r runtime $@
