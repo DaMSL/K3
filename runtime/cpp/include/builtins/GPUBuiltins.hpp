5e495a8882b1ed1cfce44e206171e222c1143a67
Yunlong Liu <yliu120@jhmi.edu>
2015-11-12 05:03:13 +0000

    GPU Builtins

---
+++ runtime/cpp/include/builtins/GPUBuiltins.hpp
@@ -0,0 +1,38 @@
+#ifndef K3_GPUBUILTINS
+#define K3_GPUBUILTINS
+
+#include "cuda.h"
+#include "nvrtc.h"
+
+#include "types/BaseString.hpp"
+#include "Common.hpp"
+
+namespace K3 {
+
+class GPUBuiltins {
+
+public:
+  GPUBuiltins();
+
+  /* CUDA Driver API wrapper */
+  int get_device_count(unit_t) { 
+    int *count;
+    if (cuDeviceGetCount(count) == CUDA_SUCCESS)
+      return *count;
+    else
+      return -1;
+  }
+
+  unit_t compile_to_ptx(const string_impl& code, const string_impl& ptxname) {
+    
+    nvrtcProgram prog;              /* a program handle */
+    nvrtcResult  res;               /* an enumeration of result */
+
+    /* ... Runtime Compilation using NVRTC library .... */
+    return unit_t{};
+  }
+
+
+} // namespace K3
+ 
+#endif
