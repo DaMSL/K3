ac4c6a4f2af38c328ecd50d3bb1479f462ee2f02
Yunlong Liu <yliu120@jhmi.edu>
2015-12-10 15:56:36 -0500

    clean in runtime/cpp

--- runtime/cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home-4/yliu120@jhu.edu/K3/K3/runtime/cpp
-# Build directory: /home-4/yliu120@jhu.edu/K3/K3/runtime/cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(include/external/gtest-1.7.0)
