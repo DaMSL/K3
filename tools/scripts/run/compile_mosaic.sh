211bd89cc2df1540b4a31a25798fe5da392b6581
root <root@2599bd650330>
2016-01-18 20:53:13 +0000

    scripts/compile_mosaic: add jemalloc by default

---
+++ tools/scripts/run/compile_mosaic.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+# Command for compiling cpp file in k3
+# Since it's a little tricky to come up with the full command line, you can just use this
+# You might need to adjust it to suit your system
+export SNDPATH=`find .cabal-sandbox -name "*packages.conf.d"`
+
+# Set a default c++ compiler
+if [ -z "$CXX" ]; then
+  CXX="--gcc"
+fi
+
+echo "K3_CXXFLAGS: ${K3_CXXFLAGS}"
+
+BSLINCLUDE="-I/usr/local/include/bsl -I/usr/local/include/bdl"
+
+dist/build/k3/k3 \
+  -I lib/k3 -I examples/sql -I examples/distributed/amplab/ \
+  --mpargs package-db=$SNDPATH --mpsearch src $K3MPARGS \
+  compile \
+    $CXX -l cpp \
+    --cpp-flags="-DK3MESSAGETRACE -DK3GLOBALTRACE -DK3TRIGGERTIMES -DBOOST_LOG_DYN_LINK -DYAS_SERIALIZE_BOOST_TYPES=1 -Iruntime/cpp/include/ -Iruntime/cpp/include/external ${BSLINCLUDE} ${K3_CXXFLAGS} -lboost_serialization -lboost_system -lboost_regex -lboost_thread -lyaml-cpp -lpthread -lboost_log_setup -lboost_log -lboost_program_options -lcsvpp -ldynamic -lbsl -lbdl -ljemalloc -ftemplate-depth-1024 -O4" \
+    -r runtime -j 16 $@
