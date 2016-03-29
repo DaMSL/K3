fcab590f857d876ed81cec6a866a3b9af0f06336
root <root@qp3.damsl.cs.jhu.edu>
2016-03-29 17:20:15 +0000

    mosaic/plot_all: fixes

--- tools/scripts/mosaic/plots/plot_all.py
+++ tools/scripts/mosaic/plots/plot_all.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # Plot the output of process-all-latencies.rb
 
+import numpy
 import matplotlib.pyplot as plt
 import argparse
 import yaml
 import os
 
 queries = ['1', '3', '4', '6', '11a', '12', '17']
-nodes = [1, 4, 8, 16, 31]
+nodes = [1, 2, 4, 8, 16, 31]
 scale_factors = [0.1, 1, 10, 100]
 
 tuple_sizes = {0.1: 8 * 10**5, 1: 8 * 10**6, 10: 8 * 10**7, 100: 8*10**8}
@@ -42,7 +43,7 @@ def scalability(args, r_file, workdir):
     if not os.path.exists(out_path):
         os.mkdir(out_path)
 
-    # create plots per query
+    # create plots per query, per sf
     for q,v1 in results2.iteritems():
         # New Figure per query
         plt.figure()
@@ -50,16 +51,45 @@ def scalability(args, r_file, workdir):
 
         # A line per scale factor
         for sf, v2 in v1.iteritems():
-            plt.plot(nodes, [v2[n] for n in nodes], label="Scale Factor {}".format(sf))
+            plt.plot(nodes, [v2[n] for n in nodes], marker='o', label="Scale Factor {}".format(sf))
 
         # Labels, etc.
+        plt.grid(True)
         plt.legend(loc='lower right')
         plt.title("Query {} Throughput".format(q))
         plt.xlabel("Worker Nodes")
         plt.ylabel("Tuples/sec")
 
         # Save to file
-        plt.savefig(os.path.join(out_path, "q{}_throughput.png".format(q)))
+        plt.savefig(os.path.join(out_path, "q{}_nd_xput.png".format(q)))
+        plt.close()
+
+    # create plots per query, per nodes
+    for q,v1 in results2.iteritems():
+        # New Figure per query
+        plt.figure()
+        f, ax = plt.subplots()
+
+        # invert the data
+        res = {n:{sf:None for sf in scale_factors} for n in nodes}
+        for sf, v2 in v1.iteritems():
+            for n, val in v2.iteritems():
+                res[n][sf] = val
+
+        for n, v2 in res.iteritems():
+            plt.plot(scale_factors, [v2[sf] for sf in scale_factors], marker='o', label="{} Nodes".format(n))
+
+        # Labels, etc.
+        plt.grid(True)
+        plt.legend(loc='lower right')
+        plt.title("Query {} Throughput".format(q))
+        plt.xlabel("Scale Factor")
+        plt.ylabel("Tuples/sec")
+        ax.set_xscale('log')
+
+        # Save to file
+        plt.savefig(os.path.join(out_path, "q{}_sf_xput.png".format(q)))
+        plt.close()
 
 def main():
     parser = argparse.ArgumentParser()
@@ -76,11 +106,11 @@ def main():
         results = yaml.load(f)
 
     workdir = os.path.dirname(args.results_file)
-    print("workdir {}".format(workdir))
+    print("workdir={}".format(workdir))
 
     tests = results[':tests']
     if args.experiment == 's':
-        scalability(args, results, workdir)
+        scalability(args, tests, workdir)
 
 
 if __name__ == '__main__':
